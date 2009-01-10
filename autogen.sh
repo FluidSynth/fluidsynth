3fd18a3ce721c2a814cd95b1a037f348670a15dd
Pedro Lopez-Cabanillas <pedro.lopez.cabanillas@gmail.com>
2009-01-10 19:01:20 +0000

    Reverted (wrong) changes in autogen.sh for MacOSX. Added missing records to ChangeLog

--- autogen.sh
+++ autogen.sh
@@ -1,18 +1,12 @@
 #!/bin/sh
 
-# Add flags for aclocal if needed (Mac OSX)
-ACLOCAL_PATH=""
-uname -s | grep -q Darwin
-if [ $? -eq 0 ]; then
-    ACLOCAL_FLAGS=-I/sw/share/aclocal
-fi
-
 # Some poor souls have linux distributions, that don't install pkg-config by default. 
 #pkg-config --version does actually nothing, but it will fail and give 'sort of' an error message...
 
 pkg-config --version > /dev/null    \
- && aclocal $ACLOCAL_FLAGS          \
+ && aclocal                         \
  && libtoolize -f                   \
  && autoheader                      \
  && autoconf                        \
  && automake -a
+
