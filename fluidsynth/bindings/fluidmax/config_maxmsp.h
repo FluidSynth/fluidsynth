bb3e77e5ab17a8613f319c5ce5cfd4dd92d91caf
Norbert Schnell <norbert>
2009-01-16 23:09:57 +0000

    Adapted to current sources and added build projects.

--- fluidsynth/bindings/fluidmax/config_maxmsp.h
+++ fluidsynth/bindings/fluidmax/config_maxmsp.h
@@ -1,16 +1,12 @@
-#define MACINTOSH
-#define MACOS9
-
 #define HAVE_STRING_H 1
 #define HAVE_STDLIB_H 1
 #define HAVE_STDIO_H 1
 #define HAVE_MATH_H 1
 #define HAVE_STDARG_H 1
+
+#if defined(__POWERPC__)
 #define WORDS_BIGENDIAN 1
+#endif
+
 #define STDIN_FILENO 0
 #define STDOUT_FILENO 1
-
-#undef WITH_PROFILING
-
-#define __Types__
-
