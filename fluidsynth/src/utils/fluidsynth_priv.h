7ef21ceaf7a91d6bfe6c389a17f2e31ac6228f5b
David Henningsson <diwic@ubuntu.com>
2010-07-15 19:07:28 +0000

    Fixup previous commit

--- fluidsynth/src/utils/fluidsynth_priv.h
+++ fluidsynth/src/utils/fluidsynth_priv.h
@@ -165,10 +165,10 @@ typedef int fluid_socket_t;
 
 #if SUPPORTS_VLA
 #  define FLUID_DECLARE_VLA(_type, _name, _len) \
-  _type _name[_len]
+     _type _name[_len]
 #else
 #  define FLUID_DECLARE_VLA(_type, _name, _len) \
-     _type* _name = alloca(sizeof(_type) * (_len))
+     _type* _name = g_newa(_type, (_len))
 #endif
 
 
