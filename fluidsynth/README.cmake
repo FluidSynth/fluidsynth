19cd260522917249b2871251070c256d25bc10a2
Pedro Lopez-Cabanillas <pedro.lopez.cabanillas@gmail.com>
2010-12-04 09:16:14 +0000

    link to Doxygen project page

--- fluidsynth/README.cmake
+++ fluidsynth/README.cmake
@@ -71,8 +71,8 @@ There is a "clean" target, but not a "distclean" one. You should use a build
 directory different to the source tree. In this case, the "distclean" target 
 would be equivalent to simply removing the build directory. 
 
-To compile the developer documentation, use this command from the root build
-directory:
+To compile the developer documentation, install Doxygen (http://www.doxygen.org) 
+and use this command from the root build directory:
 
 $ make doxygen
 
