1f5a3ca26e5c75e3ae54eb4c88f1f2bd21f87644
derselbst <tom.mbrt@googlemail.com>
2017-07-12 17:45:23 +0200

    license upgrade
    for the sake of consistency, make a compatible license upgrade of fluidsynth from 'GNU Library GPL 2' to 'GNU Lesser GPL 2.1'

--- fluidsynth/src/sfloader/fluid_defsfont.h
+++ fluidsynth/src/sfloader/fluid_defsfont.h
@@ -5,14 +5,14 @@
  * SoundFont loading code borrowed from Smurf SoundFont Editor by Josh Green
  *
  * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Library General Public License
+ * modify it under the terms of the GNU Lesser General Public License
  * as published by the Free Software Foundation; either version 2 of
  * the License, or (at your option) any later version.
  *
  * This library is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Library General Public License for more details.
+ * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Library General Public
  * License along with this library; if not, write to the Free
@@ -318,7 +318,7 @@ SFData *sfload_file (const char * fname);
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the GNU
- * Library General Public License for more details.
+ * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Library General Public
  * License along with this library; if not, write to the
