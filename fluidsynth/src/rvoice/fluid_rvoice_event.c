6b847829efc16995841099e6a8c4be50e1a6c06c
derselbst <tom.mbrt@googlemail.com>
2017-08-27 12:48:32 +0200

    no need for atomic_int_dec_and_test()

--- fluidsynth/src/rvoice/fluid_rvoice_event.c
+++ fluidsynth/src/rvoice/fluid_rvoice_event.c
@@ -188,7 +188,7 @@ static int fluid_rvoice_eventhandler_push_LOCAL(fluid_rvoice_eventhandler_t* han
   event = fluid_ringbuffer_get_inptr(handler->queue, old_queue_stored);
 
   if (event == NULL) {
-    fluid_atomic_int_dec_and_test(&handler->queue_stored);
+    fluid_atomic_int_add(&handler->queue_stored, -1);
     FLUID_LOG(FLUID_WARN, "Ringbuffer full, try increasing polyphony!");
     return FLUID_FAILED; // Buffer full...
   }
