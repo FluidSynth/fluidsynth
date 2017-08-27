f600e7aa012ce8be94600328b679752c540604d0
derselbst <tom.mbrt@googlemail.com>
2017-08-27 21:34:43 +0200

    update doc

--- fluidsynth/include/fluidsynth/event.h
+++ fluidsynth/include/fluidsynth/event.h
@@ -53,7 +53,7 @@ enum fluid_seq_event_type {
   FLUID_SEQ_VOLUME,		/**< Volume set event */
   FLUID_SEQ_REVERBSEND,		/**< Reverb send set event */
   FLUID_SEQ_CHORUSSEND,		/**< Chorus send set event */
-  FLUID_SEQ_TIMER,		/**< Timer event (DOCME) */
+  FLUID_SEQ_TIMER,		/**< Timer event (useful for giving a callback at a certain time) */
   FLUID_SEQ_ANYCONTROLCHANGE,	/**< DOCME (used for remove_events only) */
   FLUID_SEQ_CHANNELPRESSURE,    /**< Channel aftertouch event @since 1.1.0 */
   FLUID_SEQ_SYSTEMRESET,        /**< System reset event @since 1.1.0 */
