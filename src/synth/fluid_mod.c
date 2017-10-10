68f065329acd016cd961d5f0c960b4c94c6e044b
derselbst <tom.mbrt@googlemail.com>
2017-10-10 14:10:45 +0200

    fix bipolar log calc

--- src/synth/fluid_mod.c
+++ src/synth/fluid_mod.c
@@ -289,12 +289,12 @@ fluid_mod_transform_source_value(fluid_real_t val, unsigned char mod_flags, cons
       val = log10(1 + (1.0f - val_norm) * 9);
       break;
     case FLUID_MOD_LOG | FLUID_MOD_BIPOLAR | FLUID_MOD_POSITIVE: /* custom */
-      val = (val_norm > 0.5f) ?  log10(1 + 2 * (val_norm - 0.5f)) 
-                              : -log10(1 + 2 * (0.5f - val_norm));
+      val = (val_norm > 0.5f) ?  log10(1 + 9 * 2 * (val_norm - 0.5f)) 
+                              : -log10(1 + 9 * 2 * (0.5f - val_norm));
       break;
     case FLUID_MOD_LOG | FLUID_MOD_BIPOLAR | FLUID_MOD_NEGATIVE: /* custom */
-      val = (val_norm > 0.5f) ? -log10(1 + 2 * (val_norm - 0.5f)) 
-                              :  log10(1 + 2 * (0.5f - val_norm));
+      val = (val_norm > 0.5f) ? -log10(1 + 9 * 2 * (val_norm - 0.5f)) 
+                              :  log10(1 + 9 * 2 * (0.5f - val_norm));
       break;
       
     case FLUID_MOD_SIN | FLUID_MOD_UNIPOLAR | FLUID_MOD_POSITIVE: /* custom */
