06009b058d6807ba83f1367077f8cc2ec2cbf898
jjceresa <32781294+jjceresa@users.noreply.github.com>
2017-11-06 00:39:52 +0100

    Update fluid_dll.c for dsound_audio_driver
    Window creation handle is now owned by dsound_audio_driver.
    An little enhancement allows the application to instruct dsound_audio_driver()
    to use the application window handle. 
    If  the application doesn't instruct dsound driver, the driver create/destroy it own window.

--- src/fluid_dll.c
+++ src/fluid_dll.c
@@ -24,48 +24,18 @@
 
 static HINSTANCE fluid_hinstance = NULL;
 static HWND fluid_wnd = NULL;
-static int fluid_refCount = 0;
-
-int fluid_win32_create_window(void);
-void fluid_win32_destroy_window(void);
 HWND fluid_win32_get_window(void);
 
 #ifndef FLUIDSYNTH_NOT_A_DLL
+/* Dll entry point */
 BOOL WINAPI DllMain(HANDLE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
 {
   FLUID_LOG(FLUID_DBG, "DllMain");
-  switch (ul_reason_for_call) {
-  case DLL_PROCESS_ATTACH:
-    fluid_refCount++;
-    if (1 == fluid_refCount) {
-      fluid_set_hinstance((void*) hModule);
-      fluid_win32_create_window();
-    }
-    break;
-  case DLL_PROCESS_DETACH:
-    fluid_refCount--;
-    if (fluid_refCount == 0) {
-      fluid_win32_destroy_window();
-    }
-    break;
-  }
+  fluid_hinstance = hModule;
   return TRUE;
 }
 #endif
 
-/**
- * Set the handle to the instance of the application on the Windows platform.
- * @param Application instance pointer
- *
- * The handle is needed to open DirectSound.
- */
-void fluid_set_hinstance(void* hinstance)
-{
-  if (fluid_hinstance == NULL) {
-    fluid_hinstance = (HINSTANCE) hinstance;
-    FLUID_LOG(FLUID_DBG, "DLL instance = %d", (int) fluid_hinstance);
-  }
-}
 
 /**
  * Get the handle to the instance of the application on the Windows platform.
@@ -76,53 +46,19 @@ void* fluid_get_hinstance(void)
   return (void*) fluid_hinstance;
 }
 
-static long FAR PASCAL fluid_win32_wndproc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+/* sets the application window handle
+ * @param hwnd the window handle
+ *
+ * If an application set a window handle this window will
+ * be used by dsound driver for cooperative use.
+ *
+ * It is not mandatory for an application to set a window handle
+ * in this case dsound driver will create its own window.
+ *
+*/
+void fluid_win32_set_window(HWND hwnd)
 {
-  switch (message) {
-  case WM_CREATE:
-    break;
-  case WM_DESTROY:
-    break;
-  default:
-    return DefWindowProc(hWnd, message, wParam, lParam);
-    break;
-  }
-  return(0L);
-}
-
-int fluid_win32_create_window(void)
-{
-  WNDCLASS myClass;
-  myClass.hCursor = LoadCursor( NULL, IDC_ARROW );
-  myClass.hIcon = NULL;
-  myClass.lpszMenuName = (LPSTR) NULL;
-  myClass.lpszClassName = (LPSTR) "FluidSynth";
-  myClass.hbrBackground = (HBRUSH)(COLOR_WINDOW);
-  myClass.hInstance = fluid_hinstance;
-  myClass.style = CS_GLOBALCLASS;
-  myClass.lpfnWndProc = fluid_win32_wndproc;
-  myClass.cbClsExtra = 0;
-  myClass.cbWndExtra = 0;
-  if (!RegisterClass(&myClass)) {
-    return -100;
-  }
-  fluid_wnd = CreateWindow((LPSTR) "FluidSynth", (LPSTR) "FluidSynth", WS_OVERLAPPEDWINDOW,
-              CW_USEDEFAULT, CW_USEDEFAULT, 400, 300, (HWND) NULL, (HMENU) NULL,
-              fluid_hinstance, (LPSTR) NULL);
-  if (fluid_wnd == NULL) {
-    FLUID_LOG(FLUID_ERR, "Can't create window");
-    return -101;
-  }
-  return 0;
-}
-
-void fluid_win32_destroy_window(void)
-{
-  HWND hwnd = fluid_win32_get_window();
-  if (hwnd) {
-    DestroyWindow(hwnd);
-    fluid_wnd = 0;
-  }
+    fluid_wnd = hwnd;
 }
 
 HWND fluid_win32_get_window(void)
