c2eab91ac22360e2cda433a99190de1ae021e326
carlo-bramini <carlo.bramini@users.sourceforge.net>
2017-11-05 18:20:51 +0100

    Remove VisualC++ types.

--- src/utils/fluidsynth_priv.h
+++ src/utils/fluidsynth_priv.h
@@ -116,18 +116,6 @@
 #if HAVE_STDINT_H
 #include <stdint.h>
 
-#elif defined _MSC_VER
-
-/* Emulate with standard MSVC types */
-typedef signed __int8    int8_t;
-typedef unsigned __int8  uint8_t;
-typedef signed __int16   int16_t;
-typedef unsigned __int16 uint16_t;
-typedef signed __int32   int32_t;
-typedef unsigned __int32 uint32_t;
-typedef signed __int64   int64_t;
-typedef unsigned __int64 uint64_t;
-
 #else
 
 /* Assume GLIB types */
