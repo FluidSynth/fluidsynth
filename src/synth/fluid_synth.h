486dfed111bce8e48b4bad59d0330e4593e92cbf
jjceresa <32781294+jjceresa@users.noreply.github.com>
2017-11-09 22:59:41 +0100

    Renaming functions

--- src/synth/fluid_synth.h
+++ src/synth/fluid_synth.h
@@ -237,9 +237,9 @@ int fluid_synth_noteoff_monopoly(fluid_synth_t* synth, int chan, int key, char M
 
 void fluid_channel_set_onenote_monolist(fluid_channel_t* chan, unsigned char key, unsigned char vel);
 void fluid_channel_clear_monolist(fluid_channel_t* chan);
-void invalid_prev_note_staccato(fluid_channel_t* chan);
-void legato_on_off(fluid_channel_t* chan, int value);
-void breath_note_on_off(fluid_channel_t* chan, int value);
+void fluid_channel_invalid_prev_note_staccato(fluid_channel_t* chan);
+void fluid_channel_cc_legato(fluid_channel_t* chan, int value);
+void fluid_channel_cc_breath_note_on_off(fluid_channel_t* chan, int value);
 
 
 #endif  /* _FLUID_SYNTH_H */
