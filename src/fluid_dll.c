c3741b6f3314695b1ef4086e73c886b11d7530b8
derselbst <tom.mbrt@googlemail.com>
2017-11-22 16:29:27 +0100

    restore minimalistic fluid_dll.c to avoid API breakage
    for 1.1.x only

---
+++ src/fluid_dll.c
@@ -0,0 +1,62 @@
+/* FluidSynth - A Software Synthesizer
+ *
+ * Copyright (C) 2003  Peter Hanappe and others.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA
+ */
+
+#ifdef WIN32
+#include "fluidsynth_priv.h"
+#include "fluid_sys.h"
+
+static HINSTANCE fluid_hinstance = NULL;
+
+
+#ifndef FLUIDSYNTH_NOT_A_DLL
+BOOL WINAPI DllMain(HANDLE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
+{
+  FLUID_LOG(FLUID_DBG, "DllMain");
+  switch (ul_reason_for_call) {
+  case DLL_PROCESS_ATTACH:
+      fluid_set_hinstance((void*) hModule);
+    break;
+  }
+  return TRUE;
+}
+#endif
+
+/**
+ * Set the handle to the instance of the application on the Windows platform.
+ * @param Application instance pointer
+ *
+ * The handle is needed to open DirectSound.
+ */
+void fluid_set_hinstance(void* hinstance)
+{
+  if (fluid_hinstance == NULL) {
+    fluid_hinstance = (HINSTANCE) hinstance;
+    FLUID_LOG(FLUID_DBG, "DLL instance = %d", (int) fluid_hinstance);
+  }
+}
+
+/**
+ * Get the handle to the instance of the application on the Windows platform.
+ * @return Application instance pointer or NULL if not set
+ */
+void* fluid_get_hinstance(void)
+{
+  return (void*) fluid_hinstance;
+}
