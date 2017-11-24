d2a3ec282fadeac1fb9c6620fe21f9cb48d82a84
Marcus Weseloh <marcus@weseloh.cc>
2017-11-24 12:30:10 +0100

    char is unsigned on some platforms/compilers (e.g. on Linaro ARM 2016.11)

--- src/midi/fluid_midi.c
+++ src/midi/fluid_midi.c
@@ -240,7 +240,7 @@ fluid_midi_file_read_mthd(fluid_midi_file *mf)
     mf->type = mthd[9];
     mf->ntracks = (unsigned) mthd[11];
     mf->ntracks += (unsigned int) (mthd[10]) << 16;
-    if ((mthd[12]) < 0) {
+    if ((mthd[12]) & (1 << 7)) {
         mf->uses_smpte = 1;
         mf->smpte_fps = -mthd[12];
         mf->smpte_res = (unsigned) mthd[13];
