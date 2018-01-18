9bf7207e36a937f088737686dc677cae037c148e
loki666 <simons.philippe@gmail.com>
2018-01-18 15:28:29 +0100

    switch to cmake

--- android/Android.mk
+++
@@ -1,9 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := fluidsynth_shared
-LOCAL_SRC_FILES := libs/$(TARGET_ARCH_ABI)/libfluidsynth.so
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../include $(LOCAL_PATH)/jni/include
-TARGET_PLATFORM = android-14
-
-include $(PREBUILT_SHARED_LIBRARY)
