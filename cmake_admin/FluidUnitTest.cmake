147dbb2aa180bcbdeb84014d0557e187f9720cab
derselbst <tom.mbrt@googlemail.com>
2018-04-06 22:36:23 +0200

    fix build

--- cmake_admin/FluidUnitTest.cmake
+++ cmake_admin/FluidUnitTest.cmake
@@ -3,7 +3,10 @@ macro ( ADD_FLUID_TEST _test )
     TARGET_LINK_LIBRARIES(${_test} libfluidsynth)
 
     # use the local include path to look for fluidsynth.h, as we cannot be sure fluidsynth is already installed
-    target_include_directories(${_test} PUBLIC $<BUILD_INTERFACE:${CMAKE_BINARY_DIR}/include>)
+    target_include_directories(${_test}
+    PUBLIC
+    $<BUILD_INTERFACE:${CMAKE_BINARY_DIR}/include>
+    $<BUILD_INTERFACE:${CMAKE_SOURCE_DIR}/include>)
 
     ADD_TEST(NAME ${_test} COMMAND ${_test})
 endmacro ( ADD_FLUID_TEST )
