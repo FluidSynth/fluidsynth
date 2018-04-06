42a6a2153aa141dc17e3cf27754f5892cf372192
derselbst <tom.mbrt@googlemail.com>
2018-04-06 22:07:21 +0200

    add a macro to simplify adding unit tests

---
+++ cmake_admin/FluidUnitTest.cmake
@@ -0,0 +1,9 @@
+macro ( ADD_FLUID_TEST _test )
+    ADD_EXECUTABLE(${_test} ${_test}.c)
+    TARGET_LINK_LIBRARIES(${_test} libfluidsynth)
+
+    # use the local include path to look for fluidsynth.h, as we cannot be sure fluidsynth is already installed
+    target_include_directories(${_test} PUBLIC $<BUILD_INTERFACE:${CMAKE_BINARY_DIR}/include>)
+
+    ADD_TEST(NAME ${_test} COMMAND ${_test})
+endmacro ( ADD_FLUID_TEST )
