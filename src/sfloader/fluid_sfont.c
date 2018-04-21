08147cc3d6ec344373601d05e598e99e2f228adb
derselbst <tom.mbrt@googlemail.com>
2018-04-21 16:33:27 +0200

    avoid double free in fluid_sample_set_sound_data()

--- src/sfloader/fluid_sfont.c
+++ src/sfloader/fluid_sfont.c
@@ -459,6 +459,7 @@ fluid_sample_set_sound_data (fluid_sample_t* sample,
     {
         FLUID_FREE(sample->data);
         FLUID_FREE(sample->data24);
+        sample->data = sample->data24 = NULL;
     }
 
     if (copy_data)
