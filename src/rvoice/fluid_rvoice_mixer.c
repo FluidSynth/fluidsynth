FluidSynth/fluidsynth

commit 7ae909929318cb48aebb0d551903269e0c5a6735

unify rvoice update functions calling conventions
by proper typedefing

src/rvoice/fluid_rvoice_mixer.c
@@ -311,14 +311,15 @@ static int fluid_mixer_buffers_replace_voice(fluid_mixer_buffers_t* buffers,
 }
 */
 
-int 
-fluid_rvoice_mixer_add_voice(fluid_rvoice_mixer_t* mixer, fluid_rvoice_t* voice)
+DECLARE_FLUID_RVOICE_FUNCTION(fluid_rvoice_mixer_add_voice)
 {
   int i;
+  fluid_rvoice_mixer_t* mixer = obj;
+  fluid_rvoice_t* voice = param[0].ptr;
 
   if (mixer->active_voices < mixer->polyphony) {
     mixer->rvoices[mixer->active_voices++] = voice;
-    return FLUID_OK;
+    return; // success
   }
   
   /* See if any voices just finished, if so, take its place.
@@ -326,18 +327,18 @@ fluid_rvoice_mixer_add_voice(fluid_rvoice_mixer_t* mixer, fluid_rvoice_t* voice)
   for (i=0; i < mixer->active_voices; i++) {
     if (mixer->rvoices[i] == voice) {
       FLUID_LOG(FLUID_ERR, "Internal error: Trying to replace an existing rvoice in fluid_rvoice_mixer_add_voice?!");
-      return FLUID_FAILED;
+      return;
     }
     if (mixer->rvoices[i]->envlfo.volenv.section == FLUID_VOICE_ENVFINISHED) {
       fluid_finish_rvoice(&mixer->buffers, mixer->rvoices[i]);
       mixer->rvoices[i] = voice;
-      return FLUID_OK;
+      return; // success
     }
   }
 
   /* This should never happen */
   FLUID_LOG(FLUID_ERR, "Trying to exceed polyphony in fluid_rvoice_mixer_add_voice");
-  return FLUID_FAILED;
+  return;
 }
 
 static int 
@@ -359,21 +360,23 @@ fluid_mixer_buffers_update_polyphony(fluid_mixer_buffers_t* buffers, int value)
  * Update polyphony - max number of voices (NOTE: not hard real-time capable)
  * @return FLUID_OK or FLUID_FAILED
  */
-int 
-fluid_rvoice_mixer_set_polyphony(fluid_rvoice_mixer_t* handler, int value)
+DECLARE_FLUID_RVOICE_FUNCTION(fluid_rvoice_mixer_set_polyphony)
 {
   void* newptr;
+  fluid_rvoice_mixer_t* handler = obj;
+  int value = param[0].i;
+  
   if (handler->active_voices > value) 
-    return FLUID_FAILED;
+    return /*FLUID_FAILED*/;
 
   newptr = FLUID_REALLOC(handler->rvoices, value * sizeof(fluid_rvoice_t*));
   if (newptr == NULL) 
-    return FLUID_FAILED;
+    return /*FLUID_FAILED*/;
   handler->rvoices = newptr;
 
   if (fluid_mixer_buffers_update_polyphony(&handler->buffers, value) 
       == FLUID_FAILED)
-    return FLUID_FAILED;
+    return /*FLUID_FAILED*/;
 
 #ifdef ENABLE_MIXER_THREADS
   {
@@ -381,12 +384,12 @@ fluid_rvoice_mixer_set_polyphony(fluid_rvoice_mixer_t* handler, int value)
     for (i=0; i < handler->thread_count; i++)
       if (fluid_mixer_buffers_update_polyphony(&handler->threads[i], value) 
           == FLUID_FAILED)
-        return FLUID_FAILED;
+        return /*FLUID_FAILED*/;
   }
 #endif
 
   handler->polyphony = value;
-  return FLUID_OK;
+  return /*FLUID_OK*/;
 }
 
 
@@ -497,17 +500,20 @@ fluid_mixer_buffers_init(fluid_mixer_buffers_t* buffers, fluid_rvoice_mixer_t* m
 /**
  * Note: Not hard real-time capable (calls malloc)
  */
-void 
-fluid_rvoice_mixer_set_samplerate(fluid_rvoice_mixer_t* mixer, fluid_real_t samplerate)
+DECLARE_FLUID_RVOICE_FUNCTION(fluid_rvoice_mixer_set_samplerate)
 {
   int i;
+  fluid_rvoice_mixer_t* mixer = obj;
+  fluid_real_t samplerate = param[1].real;
+  param[0].real = param[1].real; // FIXME: this is needed for fluid_rvoice_set_output_rate()
+  
   if (mixer->fx.chorus)
     delete_fluid_chorus(mixer->fx.chorus);
   mixer->fx.chorus = new_fluid_chorus(samplerate);
   if (mixer->fx.reverb)
 	  fluid_revmodel_samplerate_change(mixer->fx.reverb, samplerate);
   for (i=0; i < mixer->active_voices; i++)
-    fluid_rvoice_set_output_rate(mixer->rvoices[i], samplerate);
+    fluid_rvoice_set_output_rate(mixer->rvoices[i], param);
 #if LADSPA
   if (mixer->ladspa_fx != NULL)
   {
@@ -612,7 +618,6 @@ void delete_fluid_rvoice_mixer(fluid_rvoice_mixer_t* mixer)
 {
   fluid_return_if_fail(mixer != NULL);
   
-  fluid_rvoice_mixer_set_threads(mixer, 0, 0);
 #ifdef ENABLE_MIXER_THREADS
   if (mixer->thread_ready)
     delete_fluid_cond(mixer->thread_ready);
@@ -660,13 +665,18 @@ void fluid_rvoice_mixer_set_ladspa(fluid_rvoice_mixer_t* mixer,
 }
 #endif
 
-void fluid_rvoice_mixer_set_reverb_enabled(fluid_rvoice_mixer_t* mixer, int on)
+DECLARE_FLUID_RVOICE_FUNCTION(fluid_rvoice_mixer_set_reverb_enabled)
 {
+  fluid_rvoice_mixer_t* mixer = obj;
+  int on = param[0].i;
+  
   mixer->fx.with_reverb = on;
 }
 
-void fluid_rvoice_mixer_set_chorus_enabled(fluid_rvoice_mixer_t* mixer, int on)
+DECLARE_FLUID_RVOICE_FUNCTION(fluid_rvoice_mixer_set_chorus_enabled)
 {
+  fluid_rvoice_mixer_t* mixer = obj;
+  int on = param[0].i;
   mixer->fx.with_chorus = on;
 }
 
@@ -675,32 +685,40 @@ void fluid_rvoice_mixer_set_mix_fx(fluid_rvoice_mixer_t* mixer, int on)
   mixer->fx.mix_fx_to_out = on;
 }
 
-void fluid_rvoice_mixer_set_chorus_params(fluid_rvoice_mixer_t* mixer, int set, 
-				         int nr, fluid_real_t level, fluid_real_t speed, 
-				         fluid_real_t depth_ms, int type)
+DECLARE_FLUID_RVOICE_FUNCTION(fluid_rvoice_mixer_set_chorus_params)
 {
+  fluid_rvoice_mixer_t* mixer = obj;
+  int set = param[0].i;
+  int nr = param[1].i;
+  fluid_real_t level = param[2].real;
+  fluid_real_t speed = param[3].real;
+  fluid_real_t depth_ms = param[4].real;
+  int type = param[5].i;
+  
   fluid_chorus_set(mixer->fx.chorus, set, nr, level, speed, depth_ms, type);
 }
-void fluid_rvoice_mixer_set_reverb_params(fluid_rvoice_mixer_t* mixer, int set, 
-					 fluid_real_t roomsize, fluid_real_t damping, 
-					 fluid_real_t width, fluid_real_t level)
+
+DECLARE_FLUID_RVOICE_FUNCTION(fluid_rvoice_mixer_set_reverb_params)
 {
+    fluid_rvoice_mixer_t* mixer = obj;
+    int set = param[0].i;
+    fluid_real_t roomsize = param[1].real;
+    fluid_real_t damping = param[2].real;
+    fluid_real_t width = param[3].real;
+    fluid_real_t level = param[4].real;
+    
   fluid_revmodel_set(mixer->fx.reverb, set, roomsize, damping, width, level); 
 }
 
-void fluid_rvoice_mixer_reset_fx(fluid_rvoice_mixer_t* mixer)
+DECLARE_FLUID_RVOICE_FUNCTION(fluid_rvoice_mixer_reset_reverb)
 {
-  fluid_revmodel_reset(mixer->fx.reverb);
-  fluid_chorus_reset(mixer->fx.chorus);
-}
-
-void fluid_rvoice_mixer_reset_reverb(fluid_rvoice_mixer_t* mixer)
-{
+  fluid_rvoice_mixer_t* mixer = obj;
   fluid_revmodel_reset(mixer->fx.reverb);
 }
 
-void fluid_rvoice_mixer_reset_chorus(fluid_rvoice_mixer_t* mixer)
+DECLARE_FLUID_RVOICE_FUNCTION(fluid_rvoice_mixer_reset_chorus)
 {
+  fluid_rvoice_mixer_t* mixer = obj;
   fluid_chorus_reset(mixer->fx.chorus);
 }
 
@@ -913,13 +931,14 @@ fluid_render_loop_multithread(fluid_rvoice_mixer_t* mixer)
  * @param thread_count Number of extra mixer threads for multi-core rendering
  * @param prio_level real-time prio level for the extra mixer threads
  */
-void 
-fluid_rvoice_mixer_set_threads(fluid_rvoice_mixer_t* mixer, int thread_count, 
-  			       int prio_level)
+DECLARE_FLUID_RVOICE_FUNCTION(fluid_rvoice_mixer_set_threads)
 {
 #ifdef ENABLE_MIXER_THREADS
   char name[16];
   int i;
+  fluid_rvoice_mixer_t* mixer = obj;
+  int thread_count = param[0].i;
+  int prio_level = param[1].real;
  
   // Kill all existing threads first
   if (mixer->thread_count) {

src/rvoice/fluid_adsr_env.c
@@ -20,15 +20,16 @@
 
 #include "fluid_adsr_env.h"
 
-void 
-fluid_adsr_env_set_data(fluid_adsr_env_t* env,
-                        fluid_adsr_env_section_t section,
-                        unsigned int count,
-                        fluid_real_t coeff,
-                        fluid_real_t increment,
-                        fluid_real_t min,
-                        fluid_real_t max)
+DECLARE_FLUID_RVOICE_FUNCTION(fluid_adsr_env_set_data)
 {
+    fluid_adsr_env_t* env = obj;
+    fluid_adsr_env_section_t section = param[0].i;
+    unsigned int count = param[1].i;
+    fluid_real_t coeff = param[2].real;
+    fluid_real_t increment = param[3].real;
+    fluid_real_t min = param[4].real;
+    fluid_real_t max = param[5].real;
+    
   env->data[section].count = count;
   env->data[section].coeff = coeff;
   env->data[section].increment = increment;

src/rvoice/fluid_adsr_env.h
@@ -104,14 +104,7 @@ fluid_adsr_env_calc(fluid_adsr_env_t* env, int is_volenv)
 
 /* This one cannot be inlined since it is referenced in 
    the event queue */
-void 
-fluid_adsr_env_set_data(fluid_adsr_env_t* env,
-                        fluid_adsr_env_section_t section,
-                        unsigned int count,
-                        fluid_real_t coeff,
-                        fluid_real_t increment,
-                        fluid_real_t min,
-                        fluid_real_t max);
+DECLARE_FLUID_RVOICE_FUNCTION(fluid_adsr_env_set_data);
 
 static FLUID_INLINE void 
 fluid_adsr_env_reset(fluid_adsr_env_t* env)

src/rvoice/fluid_iir_filter.c
@@ -138,8 +138,12 @@ fluid_iir_filter_apply(fluid_iir_filter_t* iir_filter,
 }
 
 
-void fluid_iir_filter_init(fluid_iir_filter_t* iir_filter, enum fluid_iir_filter_type type, enum fluid_iir_filter_flags flags)
+DECLARE_FLUID_RVOICE_FUNCTION(fluid_iir_filter_init)
 {
+    fluid_iir_filter_t* iir_filter = obj;
+    enum fluid_iir_filter_type type = param[0].i;
+    enum fluid_iir_filter_flags flags = param[1].i;
+    
     iir_filter->type = type;
     iir_filter->flags = flags;
     if(type != FLUID_IIR_DISABLED)
@@ -158,10 +162,11 @@ fluid_iir_filter_reset(fluid_iir_filter_t* iir_filter)
   iir_filter->filter_startup = 1;
 }
 
-void 
-fluid_iir_filter_set_fres(fluid_iir_filter_t* iir_filter, 
-                          fluid_real_t fres)
+DECLARE_FLUID_RVOICE_FUNCTION(fluid_iir_filter_set_fres)
 {
+  fluid_iir_filter_t* iir_filter = obj;
+  fluid_real_t fres = param[0].real;
+  
   iir_filter->fres = fres;
   iir_filter->last_fres = -1.;
 }
@@ -197,9 +202,10 @@ static fluid_real_t fluid_iir_filter_q_from_dB(fluid_real_t q_dB)
     return pow(10.0f, q_dB / 20.0f);
 }
 
-void 
-fluid_iir_filter_set_q(fluid_iir_filter_t* iir_filter, fluid_real_t q)
+DECLARE_FLUID_RVOICE_FUNCTION(fluid_iir_filter_set_q)
 {
+    fluid_iir_filter_t* iir_filter = obj;
+    fluid_real_t q = param[0].real;
     int flags = iir_filter->flags;
     
     if(flags & FLUID_IIR_Q_ZERO_OFF && q<=0.0)

src/rvoice/fluid_iir_filter.h
@@ -25,18 +25,14 @@
 
 typedef struct _fluid_iir_filter_t fluid_iir_filter_t;
 
-
-void fluid_iir_filter_init(fluid_iir_filter_t* iir_filter, enum fluid_iir_filter_type, enum fluid_iir_filter_flags flags);
+DECLARE_FLUID_RVOICE_FUNCTION(fluid_iir_filter_init);
+DECLARE_FLUID_RVOICE_FUNCTION(fluid_iir_filter_set_fres);
+DECLARE_FLUID_RVOICE_FUNCTION(fluid_iir_filter_set_q);
 
 void fluid_iir_filter_apply(fluid_iir_filter_t* iir_filter,
                             fluid_real_t *dsp_buf, int dsp_buf_count);
 
 void fluid_iir_filter_reset(fluid_iir_filter_t* iir_filter);
-
-void fluid_iir_filter_set_q(fluid_iir_filter_t* iir_filter, fluid_real_t q);
-
-void fluid_iir_filter_set_fres(fluid_iir_filter_t* iir_filter, 
-                               fluid_real_t fres);
 
 void fluid_iir_filter_calc(fluid_iir_filter_t* iir_filter, 
                            fluid_real_t output_rate, 

src/rvoice/fluid_lfo.c
@@ -1,13 +1,17 @@
 #include "fluid_lfo.h"
 
-void
-fluid_lfo_set_incr(fluid_lfo_t* lfo, fluid_real_t increment)
+DECLARE_FLUID_RVOICE_FUNCTION(fluid_lfo_set_incr)
 {
-  lfo->increment = increment;
+    fluid_lfo_t* lfo = obj;
+    fluid_real_t increment = param[0].real;
+    
+    lfo->increment = increment;
 }
 
-void
-fluid_lfo_set_delay(fluid_lfo_t* lfo, unsigned int delay)
+DECLARE_FLUID_RVOICE_FUNCTION(fluid_lfo_set_delay)
 {
-  lfo->delay = delay;
+    fluid_lfo_t* lfo = obj;
+    unsigned int delay = param[0].i;
+    
+    lfo->delay = delay;
 }

src/rvoice/fluid_lfo.h
@@ -38,8 +38,8 @@ fluid_lfo_reset(fluid_lfo_t* lfo)
 }
 
 // These two cannot be inlined since they're used by event_dispatch
-void fluid_lfo_set_incr(fluid_lfo_t* lfo, fluid_real_t increment);
-void fluid_lfo_set_delay(fluid_lfo_t* lfo, unsigned int delay);
+DECLARE_FLUID_RVOICE_FUNCTION(fluid_lfo_set_incr);
+DECLARE_FLUID_RVOICE_FUNCTION(fluid_lfo_set_delay);
 
 static FLUID_INLINE fluid_real_t
 fluid_lfo_get_val(fluid_lfo_t* lfo)

src/rvoice/fluid_rvoice.c
@@ -22,6 +22,9 @@
 #include "fluid_conv.h"
 #include "fluid_sys.h"
 
+
+static void fluid_rvoice_noteoff_LOCAL(fluid_rvoice_t* voice, unsigned int min_ticks);
+
 /**
  * @return -1 if voice has finished, 0 if it's currently quiet, 1 otherwise
  */
@@ -158,7 +161,7 @@ fluid_rvoice_check_sample_sanity(fluid_rvoice_t* voice)
 
     /* Zero length? */
     if (voice->dsp.start == voice->dsp.end){
-	fluid_rvoice_voiceoff(voice);
+	fluid_rvoice_voiceoff(voice, NULL);
 	return;
     }
 
@@ -282,7 +285,7 @@ fluid_rvoice_write (fluid_rvoice_t* voice, fluid_real_t *dsp_buf)
 
   if (voice->envlfo.noteoff_ticks != 0 && 
       voice->envlfo.ticks >= voice->envlfo.noteoff_ticks) {
-    fluid_rvoice_noteoff(voice, 0);
+    fluid_rvoice_noteoff_LOCAL(voice, 0);
   }
 
   voice->envlfo.ticks += FLUID_BUFSIZE;
@@ -475,28 +478,33 @@ fluid_rvoice_buffers_check_bufnum(fluid_rvoice_buffers_t* buffers, unsigned int 
 }
 
 
-void 
-fluid_rvoice_buffers_set_amp(fluid_rvoice_buffers_t* buffers, 
-                             unsigned int bufnum, fluid_real_t value)
+DECLARE_FLUID_RVOICE_FUNCTION(fluid_rvoice_buffers_set_amp)
 {
+  fluid_rvoice_buffers_t* buffers = obj;
+  unsigned int bufnum = param[0].i;
+  fluid_real_t value = param[1].real;
+  
   if (fluid_rvoice_buffers_check_bufnum(buffers, bufnum) != FLUID_OK)
     return;
   buffers->bufs[bufnum].amp = value;
 }
 
-void 
-fluid_rvoice_buffers_set_mapping(fluid_rvoice_buffers_t* buffers, 
-                                 unsigned int bufnum, int mapping)
+DECLARE_FLUID_RVOICE_FUNCTION(fluid_rvoice_buffers_set_mapping)
 {
+  fluid_rvoice_buffers_t* buffers = obj;
+  unsigned int bufnum = param[0].i;
+  int mapping = param[1].i;
+  
   if (fluid_rvoice_buffers_check_bufnum(buffers, bufnum) != FLUID_OK)
     return;
   buffers->bufs[bufnum].mapping = mapping;
 }
 
 
-void
-fluid_rvoice_reset(fluid_rvoice_t* voice)
+DECLARE_FLUID_RVOICE_FUNCTION(fluid_rvoice_reset)
 {
+  fluid_rvoice_t* voice = obj;
+  
   voice->dsp.has_looped = 0;
   voice->envlfo.ticks = 0;
   voice->envlfo.noteoff_ticks = 0;
@@ -530,9 +538,16 @@ fluid_rvoice_reset(fluid_rvoice_t* voice)
   voice->dsp.check_sample_sanity_flag |= FLUID_SAMPLESANITY_STARTUP;
 }
 
+DECLARE_FLUID_RVOICE_FUNCTION(fluid_rvoice_noteoff)
+{
+    fluid_rvoice_t* rvoice = obj;
+    unsigned int min_ticks = param[0].i;
+    
+    fluid_rvoice_noteoff_LOCAL(rvoice, min_ticks);
+}
 
-void 
-fluid_rvoice_noteoff(fluid_rvoice_t* voice, unsigned int min_ticks)
+static void 
+fluid_rvoice_noteoff_LOCAL(fluid_rvoice_t* voice, unsigned int min_ticks)
 {
   if (min_ticks > voice->envlfo.ticks) {
     /* Delay noteoff */
@@ -600,9 +615,9 @@ static FLUID_INLINE void fluid_rvoice_local_retrigger_attack (fluid_rvoice_t* vo
  *  see fluid_synth_noteon_mono_legato_multi_retrigger() 
  * @param voice the synthesis voice to be updated
 */
-void 
-fluid_rvoice_multi_retrigger_attack (fluid_rvoice_t* voice)
+DECLARE_FLUID_RVOICE_FUNCTION(fluid_rvoice_multi_retrigger_attack)
 {
+    fluid_rvoice_t* voice = obj;
 	int section = fluid_adsr_env_get_section(&voice->envlfo.volenv);
 	/*-------------------------------------------------------------------------
 	 Section skip for volume envelope 
@@ -645,9 +660,12 @@ fluid_rvoice_multi_retrigger_attack (fluid_rvoice_t* voice)
  *   pitchoffset is accumulated in current dsp pitchoffset.
  * 2) And to get constant portamento duration, dsp pitch increment is updated.
 */  
-void fluid_rvoice_set_portamento(fluid_rvoice_t * voice, unsigned int countinc,
-                                 fluid_real_t pitchoffset)
+DECLARE_FLUID_RVOICE_FUNCTION(fluid_rvoice_set_portamento)
 {
+    fluid_rvoice_t * voice = obj;
+    unsigned int countinc = param[0].i;
+    fluid_real_t pitchoffset = param[1].real;
+    
 	if (countinc)
 	{
 		voice->dsp.pitchoffset += pitchoffset;
@@ -657,82 +675,110 @@ void fluid_rvoice_set_portamento(fluid_rvoice_t * voice, unsigned int countinc,
 	dsp.pitchoffset will be incremented by dsp pitchinc. */
 }
 
-void 
-fluid_rvoice_set_output_rate(fluid_rvoice_t* voice, fluid_real_t value)
+
+DECLARE_FLUID_RVOICE_FUNCTION(fluid_rvoice_set_output_rate)
 {
+  fluid_rvoice_t* voice = obj;
+  fluid_real_t value = param[0].real;
+  
   voice->dsp.output_rate = value;
 }
 
-void 
-fluid_rvoice_set_interp_method(fluid_rvoice_t* voice, int value)
+DECLARE_FLUID_RVOICE_FUNCTION(fluid_rvoice_set_interp_method)
 {
+  fluid_rvoice_t* voice = obj;
+  int value = param[0].i;
+  
   voice->dsp.interp_method = value;
 }
 
-void 
-fluid_rvoice_set_root_pitch_hz(fluid_rvoice_t* voice, fluid_real_t value)
+DECLARE_FLUID_RVOICE_FUNCTION(fluid_rvoice_set_root_pitch_hz)
 {
+  fluid_rvoice_t* voice = obj;
+  fluid_real_t value = param[0].real;
+  
   voice->dsp.root_pitch_hz = value;
 }
 
-void 
-fluid_rvoice_set_pitch(fluid_rvoice_t* voice, fluid_real_t value)
+DECLARE_FLUID_RVOICE_FUNCTION(fluid_rvoice_set_pitch)
 {
+  fluid_rvoice_t* voice = obj;
+  fluid_real_t value = param[0].real;
+  
   voice->dsp.pitch = value;
 }
 
 
-void 
-fluid_rvoice_set_attenuation(fluid_rvoice_t* voice, fluid_real_t value)
+DECLARE_FLUID_RVOICE_FUNCTION(fluid_rvoice_set_attenuation)
 {
+  fluid_rvoice_t* voice = obj;
+  fluid_real_t value = param[0].real;
+  
   voice->dsp.prev_attenuation = voice->dsp.attenuation;
   voice->dsp.attenuation = value;
 }
 
-void 
-fluid_rvoice_set_min_attenuation_cB(fluid_rvoice_t* voice, fluid_real_t value)
+DECLARE_FLUID_RVOICE_FUNCTION(fluid_rvoice_set_min_attenuation_cB)
 {
+  fluid_rvoice_t* voice = obj;
+  fluid_real_t value = param[0].real;
+  
   voice->dsp.min_attenuation_cB = value;
 }
 
-void 
-fluid_rvoice_set_viblfo_to_pitch(fluid_rvoice_t* voice, fluid_real_t value)
+DECLARE_FLUID_RVOICE_FUNCTION(fluid_rvoice_set_viblfo_to_pitch)
 {
+  fluid_rvoice_t* voice = obj;
+  fluid_real_t value = param[0].real;
+  
   voice->envlfo.viblfo_to_pitch = value;
 }
 
-void fluid_rvoice_set_modlfo_to_pitch(fluid_rvoice_t* voice, fluid_real_t value)
+DECLARE_FLUID_RVOICE_FUNCTION(fluid_rvoice_set_modlfo_to_pitch)
 {
+  fluid_rvoice_t* voice = obj;
+  fluid_real_t value = param[0].real;
+  
   voice->envlfo.modlfo_to_pitch = value;
 }
 
-void 
-fluid_rvoice_set_modlfo_to_vol(fluid_rvoice_t* voice, fluid_real_t value)
+DECLARE_FLUID_RVOICE_FUNCTION(fluid_rvoice_set_modlfo_to_vol)
 {
+  fluid_rvoice_t* voice = obj;
+  fluid_real_t value = param[0].real;
+  
   voice->envlfo.modlfo_to_vol = value;
 }
 
-void 
-fluid_rvoice_set_modlfo_to_fc(fluid_rvoice_t* voice, fluid_real_t value)
+DECLARE_FLUID_RVOICE_FUNCTION(fluid_rvoice_set_modlfo_to_fc)
 {
+  fluid_rvoice_t* voice = obj;
+  fluid_real_t value = param[0].real;
+  
   voice->envlfo.modlfo_to_fc = value;
 }
 
-void 
-fluid_rvoice_set_modenv_to_fc(fluid_rvoice_t* voice, fluid_real_t value)
+DECLARE_FLUID_RVOICE_FUNCTION(fluid_rvoice_set_modenv_to_fc)
 {
+  fluid_rvoice_t* voice = obj;
+  fluid_real_t value = param[0].real;
+  
   voice->envlfo.modenv_to_fc = value;
 }
 
-void 
-fluid_rvoice_set_modenv_to_pitch(fluid_rvoice_t* voice, fluid_real_t value)
+DECLARE_FLUID_RVOICE_FUNCTION(fluid_rvoice_set_modenv_to_pitch)
 {
+  fluid_rvoice_t* voice = obj;
+  fluid_real_t value = param[0].real;
+  
   voice->envlfo.modenv_to_pitch = value;
 }
 
-void 
-fluid_rvoice_set_synth_gain(fluid_rvoice_t* voice, fluid_real_t value)
+DECLARE_FLUID_RVOICE_FUNCTION(fluid_rvoice_set_synth_gain)
 {
+  fluid_rvoice_t* voice = obj;
+  fluid_real_t value = param[0].real;
+  
   voice->dsp.synth_gain = value;
 
   /* For a looped sample, this value will be overwritten as soon as the
@@ -744,52 +790,67 @@ fluid_rvoice_set_synth_gain(fluid_rvoice_t* voice, fluid_real_t value)
   voice->dsp.check_sample_sanity_flag |= FLUID_SAMPLESANITY_CHECK;
 }
 
-void 
-fluid_rvoice_set_start(fluid_rvoice_t* voice, int value)
+DECLARE_FLUID_RVOICE_FUNCTION(fluid_rvoice_set_start)
 {
+  fluid_rvoice_t* voice = obj;
+  int value = param[0].i;
+  
   voice->dsp.start = value;
   voice->dsp.check_sample_sanity_flag |= FLUID_SAMPLESANITY_CHECK;
 }
 
-void 
-fluid_rvoice_set_end(fluid_rvoice_t* voice, int value)
+DECLARE_FLUID_RVOICE_FUNCTION(fluid_rvoice_set_end)
 {
+  fluid_rvoice_t* voice = obj;
+  int value = param[0].i;
+  
   voice->dsp.end = value;
   voice->dsp.check_sample_sanity_flag |= FLUID_SAMPLESANITY_CHECK;
 }
 
-void 
-fluid_rvoice_set_loopstart(fluid_rvoice_t* voice, int value)
+DECLARE_FLUID_RVOICE_FUNCTION(fluid_rvoice_set_loopstart)
 {
+  fluid_rvoice_t* voice = obj;
+  int value = param[0].i;
+  
   voice->dsp.loopstart = value;
   voice->dsp.check_sample_sanity_flag |= FLUID_SAMPLESANITY_CHECK;
 }
 
-void fluid_rvoice_set_loopend(fluid_rvoice_t* voice, int value)
+DECLARE_FLUID_RVOICE_FUNCTION(fluid_rvoice_set_loopend)
 {
+  fluid_rvoice_t* voice = obj;
+  int value = param[0].i;
+  
   voice->dsp.loopend = value;
   voice->dsp.check_sample_sanity_flag |= FLUID_SAMPLESANITY_CHECK;
 }
 
-void fluid_rvoice_set_samplemode(fluid_rvoice_t* voice, enum fluid_loop value)
+DECLARE_FLUID_RVOICE_FUNCTION(fluid_rvoice_set_samplemode)
 {
+  fluid_rvoice_t* voice = obj;
+  enum fluid_loop value = param[0].i;
+  
   voice->dsp.samplemode = value;
   voice->dsp.check_sample_sanity_flag |= FLUID_SAMPLESANITY_CHECK;
 }
 
 
-void 
-fluid_rvoice_set_sample(fluid_rvoice_t* voice, fluid_sample_t* value)
+DECLARE_FLUID_RVOICE_FUNCTION(fluid_rvoice_set_sample)
 {
+  fluid_rvoice_t* voice = obj;
+  fluid_sample_t* value = param[0].ptr;
+  
   voice->dsp.sample = value;
   if (value) {
     voice->dsp.check_sample_sanity_flag |= FLUID_SAMPLESANITY_STARTUP;
   }
 }
 
-void 
-fluid_rvoice_voiceoff(fluid_rvoice_t* voice)
+DECLARE_FLUID_RVOICE_FUNCTION(fluid_rvoice_voiceoff)
 {
+  fluid_rvoice_t* voice = obj;
+    
   fluid_adsr_env_set_section(&voice->envlfo.volenv, FLUID_VOICE_ENVFINISHED);
   fluid_adsr_env_set_section(&voice->envlfo.modenv, FLUID_VOICE_ENVFINISHED);
 }

commit 2ad96d020e4bb2653a6650bd7302facee16d9f11

avoid leaking rvoice_mixer threads
introduced in 7ae909929318cb48aebb0d551903269e0c5a6735

src/rvoice/fluid_rvoice_mixer.c
@@ -99,6 +99,8 @@ struct _fluid_rvoice_mixer_t {
 #endif
 };
 
+static void delete_rvoice_mixer_threads(fluid_rvoice_mixer_t* mixer);
+
 static FLUID_INLINE void 
 fluid_rvoice_mixer_process_fx(fluid_rvoice_mixer_t* mixer)
 {
@@ -616,6 +618,7 @@ void delete_fluid_rvoice_mixer(fluid_rvoice_mixer_t* mixer)
 {
   fluid_return_if_fail(mixer != NULL);
   
+  delete_rvoice_mixer_threads(mixer);
 #ifdef ENABLE_MIXER_THREADS
   if (mixer->thread_ready)
     delete_fluid_cond(mixer->thread_ready);
@@ -922,24 +925,9 @@ fluid_render_loop_multithread(fluid_rvoice_mixer_t* mixer)
   //	    mixer->current_blockcount, test, mixer->active_voices, waits);
 }
 
-#endif
-
-/**
- * Update amount of extra mixer threads. 
- * @param thread_count Number of extra mixer threads for multi-core rendering
- * @param prio_level real-time prio level for the extra mixer threads
- */
-DECLARE_FLUID_RVOICE_FUNCTION(fluid_rvoice_mixer_set_threads)
+static void delete_rvoice_mixer_threads(fluid_rvoice_mixer_t* mixer)
 {
-#ifdef ENABLE_MIXER_THREADS
-  char name[16];
-  int i;
-  fluid_rvoice_mixer_t* mixer = obj;
-  int thread_count = param[0].i;
-  int prio_level = param[1].real;
- 
-  // Kill all existing threads first
-  if (mixer->thread_count) {
+    int i;
     fluid_atomic_int_set(&mixer->threads_should_terminate, 1);
     // Signal threads to wake up
     fluid_cond_mutex_lock(mixer->wakeup_threads_m);
@@ -958,6 +946,27 @@ DECLARE_FLUID_RVOICE_FUNCTION(fluid_rvoice_mixer_set_threads)
     FLUID_FREE(mixer->threads);
     mixer->thread_count = 0;
     mixer->threads = NULL;
+}
+#endif
+
+/**
+ * Update amount of extra mixer threads. 
+ * @param thread_count Number of extra mixer threads for multi-core rendering
+ * @param prio_level real-time prio level for the extra mixer threads
+ */
+DECLARE_FLUID_RVOICE_FUNCTION(fluid_rvoice_mixer_set_threads)
+{
+#ifdef ENABLE_MIXER_THREADS
+  char name[16];
+  int i;
+  fluid_rvoice_mixer_t* mixer = obj;
+  int thread_count = param[0].i;
+  int prio_level = param[1].real;
+ 
+  // Kill all existing threads first
+  if (mixer->thread_count)
+  {
+      delete_rvoice_mixer_threads(mixer);
   }
   
   if (thread_count == 0) 