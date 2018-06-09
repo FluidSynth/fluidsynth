1a4bfb52ac8fe618dc35e612f1a9ecd5d426b771
jjceresa <jjc_fluid@orange.fr>
2018-06-10 01:21:16 +0200

    introduce ATTENUATION_RANGE_CB macro: - this allows to maintain consistent dependency between   different part of code using this value.

--- src/rvoice/fluid_rvoice.h
+++ src/rvoice/fluid_rvoice.h
@@ -41,6 +41,36 @@ typedef struct _fluid_rvoice_t fluid_rvoice_t;
  */
 #define FLUID_NOISE_FLOOR 2.e-7
 
+/*
+ Attenuation range in centibels.
+ Attenuation range is the dynamic range of the volume envelope generator
+ from 0 to the end of attack segment.
+ fluidsynth is a 24 bit synth, it could (should??) be 144 dB of attenuation.
+ However the spec makes no distinction between 16 or 24 bit synths, so use
+ 96 dB here. 
+ 
+ Note about usefullnes of 24 bits:
+ 1)Even fluidsynth is a 24 bit synth, this format is only relevant if
+ the sample format coming from the soundfont is 24 bits and the audio sample format
+ choosen by the application (audio.sample.format) is not 16 bits.
+
+ 2)When the the soundfont have only 16 bits sample, the internal 24 bits number
+ have 16 bits msb and lsb to 0. Consequently, at the DAC output the dynamic range of
+ this 24 bit sample is reduced to the the dynamic of a 16 bits sample (ie 90 db)
+ even if this sample is produced by the audio driver using an audio sample format
+ compatible for a 24 bit DAC.
+
+ 3)When the audio sample format settings is 16 bits (audio.sample.format), the
+ audio driver will make use of a 16 bit DAC, and the dynamic will be reduced to 96 dB
+ even if the initial sample comes from a 24 bits soundfont.
+
+ In both cases (2) and (3). The real dynamic range is only 96 dB.
+
+ Also for case (1) FLUID_NOISE_FLOOR should be the noise floor for 24 bits (i.e -138 dB).
+ for case (2) and (3) FLUID_NOISE_FLOOR should be the noise floor for 16 bits (i.e -90 dB).
+ */
+#define ATTENUATION_RANGE_CB  960.0f
+
 
 enum fluid_loop {
   FLUID_UNLOOPED = 0,
