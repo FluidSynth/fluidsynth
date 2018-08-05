521e5451fc2922e5e0137d0e4cdb2d00b9d0a413
derselbst <tom.mbrt@gmail.com>
2018-08-05 17:55:07 +0200

    remove cmake option enable-tests
    Always build unit tests instead and use cmakes OBJECT library to bypass visibility control and the value of BUILD_SHARED_LIBS.

--- cmake_admin/FluidUnitTest.cmake
+++ cmake_admin/FluidUnitTest.cmake
@@ -1,5 +1,5 @@
 macro ( ADD_FLUID_TEST _test )
-    ADD_EXECUTABLE(${_test} ${_test}.c)
+    ADD_EXECUTABLE(${_test} ${_test}.c $<TARGET_OBJECTS:libfluidsynth-OBJ> )
     TARGET_LINK_LIBRARIES(${_test} libfluidsynth)
 
     # use the local include path to look for fluidsynth.h, as we cannot be sure fluidsynth is already installed
