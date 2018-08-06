ff1482ec0644a13af81fd56abe9a37359b029cb1
derselbst <tom.mbrt@googlemail.com>
2018-08-06 20:18:57 +0200

    fix dll visibility on win32 for unittests

--- cmake_admin/FluidUnitTest.cmake
+++ cmake_admin/FluidUnitTest.cmake
@@ -4,6 +4,8 @@ macro ( ADD_FLUID_TEST _test )
     # only build this unit test when explicitly requested by "make check"
     set_target_properties(${_test} PROPERTIES EXCLUDE_FROM_ALL TRUE)
     
+    # import necessary compile flags and dependency libraries
+    set_target_properties (${_test} PROPERTIES COMPILE_FLAGS $<TARGET_PROPERTY:fluidsynth,COMPILE_FLAGS> )
     TARGET_LINK_LIBRARIES(${_test} $<TARGET_PROPERTY:libfluidsynth,INTERFACE_LINK_LIBRARIES>)
 
     # use the local include path to look for fluidsynth.h, as we cannot be sure fluidsynth is already installed
