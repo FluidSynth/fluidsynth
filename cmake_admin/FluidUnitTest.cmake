a6b1056b8ce3f0ee55404b61cdc1b832d543d3ed
derselbst <tom.mbrt@googlemail.com>
2018-08-07 10:34:58 +0200

    fix win32 build

--- cmake_admin/FluidUnitTest.cmake
+++ cmake_admin/FluidUnitTest.cmake
@@ -5,7 +5,9 @@ macro ( ADD_FLUID_TEST _test )
     set_target_properties(${_test} PROPERTIES EXCLUDE_FROM_ALL TRUE)
     
     # import necessary compile flags and dependency libraries
-    set_target_properties (${_test} PROPERTIES COMPILE_FLAGS $<TARGET_PROPERTY:fluidsynth,COMPILE_FLAGS> )
+    if ( FLUID_CPPFLAGS )
+        set_target_properties ( ${_test} PROPERTIES COMPILE_FLAGS ${FLUID_CPPFLAGS} )
+    endif ( FLUID_CPPFLAGS )
     TARGET_LINK_LIBRARIES(${_test} $<TARGET_PROPERTY:libfluidsynth,INTERFACE_LINK_LIBRARIES>)
 
     # use the local include path to look for fluidsynth.h, as we cannot be sure fluidsynth is already installed
