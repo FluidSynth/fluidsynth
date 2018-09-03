ff8616d6af4fc304c83160e14e448effe73527ae
Atsushi Eno <atsushieno@gmail.com>
2018-09-03 17:23:13 +0900

    Move all android build sources and artifacts under `android` directory.

---
+++ android/jni/Android.mk
@@ -0,0 +1,35 @@
+LOCAL_PATH := $(call my-dir)
+TARGET_PLATFORM := android-21
+
+GLIB_LIB = ../dep/$(APP_ABI)/
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := glib-2.0
+LOCAL_SRC_FILES := $(GLIB_LIB)/libglib-2.0.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := iconv
+LOCAL_SRC_FILES := $(GLIB_LIB)/libiconv.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := intl 
+LOCAL_SRC_FILES := $(GLIB_LIB)/libintl.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := fluidsynth_static
+LOCAL_SRC_FILES := ../dep/$(APP_ABI)/libfluidsynth.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+LOCAL_MODULE := fluidsynth
+
+LOCAL_STATIC_LIBRARIES := glib-2.0 iconv intl
+
+LOCAL_WHOLE_STATIC_LIBRARIES := fluidsynth_static
+
+LOCAL_LDLIBS := -lc -lOpenSLES -ldl -llog -landroid
+
+include $(BUILD_SHARED_LIBRARY)
+
---
+++ android/jni/Application.mk
@@ -0,0 +1,3 @@
+APP_PLATFORM := android-21
+APP_ABI := x86 x86_64 armeabi-v7a arm64-v8a
+# NDK_TOOLCHAIN_VERSION := 4.9
