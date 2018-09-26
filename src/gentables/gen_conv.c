7c5cb398df3a382664555b9709de0b983e858115
derselbst <tom.mbrt@gmail.com>
2018-09-26 22:41:08 +0200

    fix autogen tables on WIN32

--- src/gentables/gen_conv.c
+++ src/gentables/gen_conv.c
@@ -51,7 +51,7 @@ static void fluid_conversion_config(void)
 
     for(i = 1; i < FLUID_VEL_CB_SIZE - 1; i++)
     {
-        x = (-200.0 / FLUID_PEAK_ATTENUATION) * log((double)(i * i) / ((FLUID_VEL_CB_SIZE - 1) * (FLUID_VEL_CB_SIZE - 1))) / log(10.0);
+        x = (-200.0 / FLUID_PEAK_ATTENUATION) * log((double)(i * i) / ((FLUID_VEL_CB_SIZE - 1) * (FLUID_VEL_CB_SIZE - 1))) / M_LN10;
         fluid_convex_tab[i] = (1.0 - x);
         fluid_concave_tab[(FLUID_VEL_CB_SIZE - 1) - i] =  x;
     }
