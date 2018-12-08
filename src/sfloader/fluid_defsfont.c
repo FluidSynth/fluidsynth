c74b080613028a33f6b5ae174ebb7bead5327889
jjceresa <jjc_fluid@orange.fr>
2018-12-08 18:24:32 +0100

    leftover in defpreset_noteon_add_mod_to_voice - this was forgotten in commit fcd6ec6.

--- src/sfloader/fluid_defsfont.c
+++ src/sfloader/fluid_defsfont.c
@@ -748,7 +748,7 @@ fluid_defpreset_noteon_add_mod_to_voice(fluid_voice_t* voice,
 
         for(i = 0; i < identity_limit_count; i++)
         {
-            if(mod_list[i] && fluid_mod_test_identity(global_mod, mod_list[i]))
+            if(fluid_mod_test_identity(global_mod, mod_list[i]))
             {
                 break;
             }
