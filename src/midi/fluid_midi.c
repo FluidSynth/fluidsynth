06ef8a4e0974aa152c8d3eea855522de4c89b8e9
derselbst <tom.mbrt@googlemail.com>
2019-02-01 13:10:52 +0100

    fluid_is_midifile(): only accept regular files

--- src/midi/fluid_midi.c
+++ src/midi/fluid_midi.c
@@ -102,6 +102,13 @@ int fluid_is_midifile(const char *filename)
         {
             return retcode;
         }
+        
+        // file exists and we have permission to open it
+        // now, test whether it's acutally a regular file or a symlink to such
+        if(!fluid_file_test(filename, G_FILE_TEST_IS_REGULAR))
+        {
+            break;
+        }
 
         if(FLUID_FREAD(&id, sizeof(id), 1, fp) != 1)
         {
