9c15e88e94c9c589af81fa10840347878ca2bba8
derselbst <tom.mbrt@googlemail.com>
2019-02-22 08:59:14 +0100

    attempt to fix mingw build

--- src/utils/fluid_sys.h
+++ src/utils/fluid_sys.h
@@ -97,7 +97,8 @@
 #include <limits.h>
 #endif
 
-#if HAVE_PTHREAD_H
+#if HAVE_PTHREAD_H && !defined(WIN32)
+// Do not include pthread on windows. It includes winsock.h, which collides with ws2tcpip.h below.
 #include <pthread.h>
 #endif
 
