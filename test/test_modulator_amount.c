6db83ee5e26ef9babe3d7fcbae17271c485656b0
derselbst <tom.mbrt@googlemail.com>
2019-07-10 16:11:50 +0200

    fix test_modulator_amount
    by using fluid_voice_add_mod()

--- test/test_modulator_amount.c
+++ test/test_modulator_amount.c
@@ -17,11 +17,10 @@ int main(void)
     fluid_channel_t *ch = new_fluid_channel(synth, 0);
     fluid_voice_t *v = new_fluid_voice(NULL, 22050);
     
-    fluid_mod_t *mod0 = &v->mod[0];
-    fluid_mod_t *mod1 = &v->mod[1];
-    fluid_mod_t *mod2 = &v->mod[2];
-    fluid_mod_t *mod3 = &v->mod[3];
-    v->mod_count = 3;
+    fluid_mod_t *mod0 = new_fluid_mod();
+    fluid_mod_t *mod1 = new_fluid_mod();
+    fluid_mod_t *mod2 = new_fluid_mod();
+    fluid_mod_t *mod3 = new_fluid_mod();
     
     fluid_channel_set_cc(ch, CC, 127);
     v->channel = ch;
@@ -44,16 +43,20 @@ int main(void)
         fluid_mod_set_amount (mod2, 300);
         fluid_mod_set_dest   (mod2, FLUID_MOD_LINK_DEST | 0);
         
+        fluid_voice_add_mod(v, mod0, FLUID_VOICE_OVERWRITE);
+        fluid_voice_add_mod(v, mod1, FLUID_VOICE_OVERWRITE);
+        fluid_voice_add_mod(v, mod2, FLUID_VOICE_OVERWRITE);
+        
         fluid_voice_calculate_modulator_contributions(v);
         
-        TEST_ASSERT(mod0->link == fluid_mod_get_amount(mod1) + fluid_mod_get_amount(mod2));
-        TEST_ASSERT(v->gen[GEN_FILTERFC].mod == mod0->link * fluid_mod_get_amount(mod0));
+        TEST_ASSERT(v->mod[0].link == fluid_mod_get_amount(mod1) + fluid_mod_get_amount(mod2));
+        TEST_ASSERT(v->gen[GEN_FILTERFC].mod == v->mod[0].link * fluid_mod_get_amount(mod0));
     }
     
     // same list, with additional mod3
     {
-        v->mod_count++;
-        v->gen[GEN_FILTERFC].mod = mod0->link = 0;
+        v->gen[GEN_FILTERFC].mod = 0;
+        v->mod_count = 0;
         
         fluid_mod_set_source1(mod1, FLUID_MOD_LINK_SRC, FLUID_MOD_GC);
         
@@ -62,12 +65,22 @@ int main(void)
         fluid_mod_set_amount (mod3, 50);
         fluid_mod_set_dest   (mod3, FLUID_MOD_LINK_DEST | 1); // link to mod1
         
+        fluid_voice_add_mod(v, mod0, FLUID_VOICE_OVERWRITE);
+        fluid_voice_add_mod(v, mod1, FLUID_VOICE_OVERWRITE);
+        fluid_voice_add_mod(v, mod2, FLUID_VOICE_OVERWRITE);
+        fluid_voice_add_mod(v, mod3, FLUID_VOICE_OVERWRITE);
+        
         fluid_voice_calculate_modulator_contributions(v);
         
-        TEST_ASSERT(mod1->link == 50);
-        TEST_ASSERT(mod0->link == fluid_mod_get_amount(mod2) + fluid_mod_get_amount(mod1) * mod1->link);
-        TEST_ASSERT(v->gen[GEN_FILTERFC].mod == mod0->link * fluid_mod_get_amount(mod0));
+        TEST_ASSERT(v->mod[1].link == fluid_mod_get_amount(mod3));
+        TEST_ASSERT(v->mod[0].link == fluid_mod_get_amount(mod2) + fluid_mod_get_amount(mod1) * v->mod[1].link);
+        TEST_ASSERT(v->gen[GEN_FILTERFC].mod == v->mod[0].link * fluid_mod_get_amount(mod0));
     }
+    
+    delete_fluid_mod(mod0);
+    delete_fluid_mod(mod1);
+    delete_fluid_mod(mod2);
+    delete_fluid_mod(mod3);
     
     delete_fluid_voice(v);
     delete_fluid_channel(ch);
