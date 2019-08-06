b584f50dcfb6ec42f138da6567176bb380bd6fd9
derselbst <tom.mbrt@googlemail.com>
2019-08-06 17:22:23 +0200

    add special treatment for mingw

--- test/test.h
+++ test/test.h
@@ -1,14 +1,23 @@
 
 #pragma once
 
+#include "config.h"
 #include <stdio.h>
 #include <stdlib.h>
 
-#if defined(WIN32)
-/* MSVC++ runtime opens a dialog when abort() is called, saying that abort() has been called...
- * This however causes the unit tests executed by the CI build to be stuck forever.
+/*
+ * We call abort() because on Linux this sends a signal to the process and causes the debugger to break.
+ *
+ * MSVC++ runtime opens a dialog when abort() is called, saying that abort() has been called and you can
+ * click "Retry" to make the debugger break.
+ * When executed by CI build however, the dialog causes the unit tests to be stuck forever.
  * Thus suppress the dialog on windows.
+ * MinGW however requires explicit linking against MSVCRT >= 8.0 for _set_abort_behavior().
+ * It's not worth the hassle to implement this with cmake...
  */
+#if defined(NO_GUI) && defined(MINGW32)
+#define TEST_ABORT exit(EXIT_FAILURE);
+#elif defined(NO_GUI) && defined(WIN32)
 #define TEST_ABORT _set_abort_behavior(0, _WRITE_ABORT_MSG); abort()
 #else
 #define TEST_ABORT abort()
