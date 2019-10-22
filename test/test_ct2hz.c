fa7354a336dd01e4d41e0a8dfe55b73322d9af58
Tom M <tom.mbrt@googlemail.com>
2019-10-22 13:09:42 +0200

    More efficient implementation of fluid_ct2hz() (#569)
    This provides a less branchy and therefore more instruction-cache-friendly version of fluid_ct2hz(), which also significantly reduces the number of floating-point comparisons.

--- test/test_ct2hz.c
+++ test/test_ct2hz.c
@@ -16,6 +16,9 @@ int main(void)
 {
     // 440 * 2^((x-6900)/1200) where x is the cent value given to ct2hz()
 
+
+    TEST_ASSERT(float_eq(fluid_ct2hz_real(38099), 2.9510849101059895e10));
+
     TEST_ASSERT(float_eq(fluid_ct2hz_real(13500), 19912.12696));
 
     TEST_ASSERT(float_eq(fluid_ct2hz_real(12900), 14080));
