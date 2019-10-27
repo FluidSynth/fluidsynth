34b4db10b7164d95e1266c713610554fa96f6914
derselbst <tom.mbrt@googlemail.com>
2019-10-27 17:07:43 +0100

    Fix broken #ifdef

--- src/utils/fluidsynth_priv.h
+++ src/utils/fluidsynth_priv.h
@@ -225,7 +225,7 @@ do { strncpy(_dst,_src,_n); \
 #define M_LN10 2.3025850929940456840179914546844
 #endif
 
-#ifdef DEBUG && !defined(NDEBUG)
+#if defined(DEBUG) && !defined(NDEBUG)
 #define FLUID_ASSERT(a) g_assert(a)
 #else
 #define FLUID_ASSERT(a)
