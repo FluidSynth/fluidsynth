78105bbb4533fa3b71a1db7ffa6b6a6528138150
derselbst <tom.mbrt@googlemail.com>
2019-12-11 13:19:35 +0100

    Remove event heap from sequencer

--- src/synth/fluid_event.h
+++ src/synth/fluid_event.h
@@ -49,39 +49,4 @@ void fluid_event_set_time(fluid_event_t *evt, unsigned int time);
 
 void fluid_event_clear(fluid_event_t *evt);
 
-/* private data for sorter + heap */
-enum fluid_evt_entry_type
-{
-    FLUID_EVT_ENTRY_INSERT = 0,
-    FLUID_EVT_ENTRY_REMOVE
-};
-
-typedef struct _fluid_evt_entry fluid_evt_entry;
-struct _fluid_evt_entry
-{
-    fluid_evt_entry *next;
-    short entryType;
-    fluid_event_t evt;
-};
-
-#define HEAP_WITH_DYNALLOC 1
-/* #undef HEAP_WITH_DYNALLOC */
-
-typedef struct _fluid_evt_heap_t
-{
-#ifdef HEAP_WITH_DYNALLOC
-    fluid_evt_entry *freelist;
-    fluid_mutex_t mutex;
-#else
-    fluid_evt_entry *head;
-    fluid_evt_entry *tail;
-    fluid_evt_entry pool;
-#endif
-} fluid_evt_heap_t;
-
-fluid_evt_heap_t *_fluid_evt_heap_init(int nbEvents);
-void _fluid_evt_heap_free(fluid_evt_heap_t *heap);
-fluid_evt_entry *_fluid_seq_heap_get_free(fluid_evt_heap_t *heap);
-void _fluid_seq_heap_set_free(fluid_evt_heap_t *heap, fluid_evt_entry *evt);
-
 #endif /* _FLUID_EVENT_PRIV_H */
