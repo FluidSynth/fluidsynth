6100980130e9fa7afc9e7d9826e2fc22c4c2a89e
derselbst <tom.mbrt@googlemail.com>
2021-03-28 17:40:34 +0200

    update zone validation test

--- test/test_sfont_zone.c
+++ test/test_sfont_zone.c
@@ -18,6 +18,12 @@ static int test_reader(void *buf, fluid_long_long_t count, void *h)
 
 static int test_seek(void *handle, fluid_long_long_t offset, int origin)
 {
+    if (origin == SEEK_CUR)
+    {
+        file_buf += offset;
+        return FLUID_OK;
+    }
+
     // shouldn't happen?
     TEST_ASSERT(0);
 }
@@ -31,8 +37,9 @@ int main(void)
 {
     // prepare a soundfont that has one preset and one instrument, with up to 3 zones each
 
-    SFData* sf = FLUID_NEW(SFData);
+    SFData *sf = FLUID_NEW(SFData);
     SFPreset *preset = FLUID_NEW(SFPreset);
+    SFInst *inst = FLUID_NEW(SFInst);
     SFZone *zone1 = FLUID_NEW(SFZone);
     SFZone *zone2 = FLUID_NEW(SFZone);
     SFZone *zone3 = FLUID_NEW(SFZone);
@@ -41,6 +48,8 @@ int main(void)
     FLUID_MEMSET(sf, 0, sizeof(*sf));
     TEST_ASSERT(preset != NULL);
     FLUID_MEMSET(preset, 0, sizeof(*preset));
+    TEST_ASSERT(inst != NULL);
+    FLUID_MEMSET(inst, 0, sizeof(*inst));
     TEST_ASSERT(zone1 != NULL);
     FLUID_MEMSET(zone1, 0, sizeof(*zone1));
     TEST_ASSERT(zone2 != NULL);
@@ -50,28 +59,129 @@ int main(void)
 
     sf->fcbs = &fcb;
     sf->preset = fluid_list_append(sf->preset, preset);
+    sf->inst = fluid_list_append(sf->inst, inst);
 
-    //preset->zone = fluid_list_prepend(preset->zone, zone3);
-    //preset->zone = fluid_list_prepend(preset->zone, zone2);
+
     preset->zone = fluid_list_prepend(preset->zone, zone1);
 
-    zone1->gen = fluid_list_prepend(zone1->gen, NULL);
-    zone1->gen = fluid_list_prepend(zone1->gen, NULL);
+    // test the good case first: one preset, with one zone, with two generators and one terminal generator
+    {
+        zone1->gen = fluid_list_prepend(zone1->gen, NULL);
+        zone1->gen = fluid_list_prepend(zone1->gen, NULL);
 
-    // test the good case first: one preset, with one zone, with two generators
-    unsigned char buf[] = { Gen_KeyRange, 0, 60, 127, Gen_VelRange, 0, 60, 127 };
-    file_buf = (unsigned char *)buf;
-    TEST_ASSERT(load_pgen(sf, sizeof(buf) / sizeof(*buf)));
+        unsigned char buf[] = { Gen_KeyRange, 0, 60, 127, Gen_VelRange, 0, 60, 127, 0,0,0,0 };
+        file_buf = (unsigned char *)buf;
+        TEST_ASSERT(load_pgen(sf, sizeof(buf) / sizeof(*buf)));
 
-    SFGen *gen = fluid_list_get(fluid_list_nth(zone1->gen, 0));
-    TEST_ASSERT(gen->id == Gen_KeyRange);
-    TEST_ASSERT(gen->amount.range.lo == 60);
-    TEST_ASSERT(gen->amount.range.hi == 127);
+        SFGen *gen = fluid_list_get(fluid_list_nth(zone1->gen, 0));
+        TEST_ASSERT(gen->id == Gen_KeyRange);
+        TEST_ASSERT(gen->amount.range.lo == 60);
+        TEST_ASSERT(gen->amount.range.hi == 127);
 
-    gen = fluid_list_get(fluid_list_nth(zone1->gen, 1));
-    TEST_ASSERT(gen->id == Gen_VelRange);
-    TEST_ASSERT(gen->amount.range.lo == 60);
-    TEST_ASSERT(gen->amount.range.hi == 127);
+        gen = fluid_list_get(fluid_list_nth(zone1->gen, 1));
+        TEST_ASSERT(gen->id == Gen_VelRange);
+        TEST_ASSERT(gen->amount.range.lo == 60);
+        TEST_ASSERT(gen->amount.range.hi == 127);
+
+        delete_fluid_list(zone1->gen);
+        zone1->gen = NULL;
+    }
+
+    // bad case: one preset, with one zone, with two similar generators
+    {
+        zone1->gen = fluid_list_prepend(zone1->gen, NULL);
+        zone1->gen = fluid_list_prepend(zone1->gen, NULL);
+
+        unsigned char buf[] = { Gen_VelRange, 0, 60, 127, Gen_VelRange, 0, 60, 127 };
+        file_buf = (unsigned char *)buf;
+        TEST_ASSERT(load_pgen(sf, sizeof(buf) / sizeof(*buf)));
+
+        SFGen *gen = fluid_list_get(fluid_list_nth(zone1->gen, 0));
+        TEST_ASSERT(gen->id == Gen_VelRange);
+        TEST_ASSERT(gen->amount.range.lo == 60);
+        TEST_ASSERT(gen->amount.range.hi == 127);
+
+        gen = fluid_list_get(fluid_list_nth(zone1->gen, 1));
+        TEST_ASSERT(gen == NULL);
+
+        delete_fluid_list(zone1->gen);
+        zone1->gen = NULL;
+    }
+
+    // bad case: one preset, with one zone, generators in wrong order
+    {
+        zone1->gen = fluid_list_prepend(zone1->gen, NULL);
+        zone1->gen = fluid_list_prepend(zone1->gen, NULL);
+        zone1->gen = fluid_list_prepend(zone1->gen, NULL);
+
+        unsigned char buf[] = { Gen_VelRange, 0, 60, 127, Gen_KeyRange, 0, 60, 127, Gen_Instrument, 0, 0xDD, 0xDD };
+        file_buf = (unsigned char *)buf;
+        TEST_ASSERT(load_pgen(sf, sizeof(buf) / sizeof(*buf)));
+
+        SFGen *gen = fluid_list_get(fluid_list_nth(zone1->gen, 0));
+        TEST_ASSERT(gen->id == Gen_VelRange);
+        TEST_ASSERT(gen->amount.range.lo == 60);
+        TEST_ASSERT(gen->amount.range.hi == 127);
+
+        gen = fluid_list_get(fluid_list_nth(zone1->gen, 1));
+        TEST_ASSERT(gen == NULL);
+        gen = fluid_list_get(fluid_list_nth(zone1->gen, 2));
+        TEST_ASSERT(gen == NULL);
+
+        TEST_ASSERT(FLUID_POINTER_TO_UINT(zone1->instsamp) == 0xDDDD + 1);
+        zone1->instsamp = NULL;
+
+        delete_fluid_list(zone1->gen);
+        zone1->gen = NULL;
+    }
+
+    // This test-case is derived from the invalid SoundFont provided in #808
+    {
+        inst->zone = fluid_list_prepend(inst->zone, zone2);
+        inst->zone = fluid_list_prepend(inst->zone, zone1);
+
+        zone1->gen = fluid_list_prepend(zone1->gen, NULL);
+        zone1->gen = fluid_list_prepend(zone1->gen, NULL);
+
+        zone2->gen = fluid_list_prepend(zone2->gen, NULL);
+        zone2->gen = fluid_list_prepend(zone2->gen, NULL);
+        zone2->gen = fluid_list_prepend(zone2->gen, NULL);
+        zone2->gen = fluid_list_prepend(zone2->gen, NULL);
+        zone2->gen = fluid_list_prepend(zone2->gen, NULL);
+
+        unsigned char buf[] =
+        {
+            Gen_ReverbSend, 0, 50, 0,
+            Gen_VolEnvRelease, 0, 206, 249,
+            Gen_KeyRange, 0, 0, 35, 
+            Gen_OverrideRootKey, 0, 43, 0,
+            Gen_StartAddrCoarseOfs, 0, 0, 0,  
+            Gen_SampleModes, 0, 1, 0,
+            Gen_StartAddrOfs, 0, 0, 0
+        };
+        file_buf = (unsigned char *)buf;
+        TEST_ASSERT(load_igen(sf, sizeof(buf) / sizeof(*buf)));
+
+        SFGen *gen = fluid_list_get(fluid_list_nth(zone1->gen, 0));
+        TEST_ASSERT(gen->id == Gen_ReverbSend);
+        TEST_ASSERT(gen->amount.range.lo == 50);
+        TEST_ASSERT(gen->amount.range.hi == 0);
+
+        gen = fluid_list_get(fluid_list_nth(zone1->gen, 1));
+        TEST_ASSERT(gen->id == Gen_VolEnvRelease);
+        TEST_ASSERT(gen->amount.range.lo == 206);
+        TEST_ASSERT(gen->amount.range.hi == 249);
+
+        gen = fluid_list_get(fluid_list_nth(zone1->gen, 2));
+        TEST_ASSERT(gen == NULL);
+
+        // zone 2 was dropped
+        TEST_ASSERT(inst->zone->next == NULL);
+
+        delete_fluid_list(zone1->gen);
+        zone1->gen = NULL;
+    }
+
 
     return EXIT_SUCCESS;
 }
