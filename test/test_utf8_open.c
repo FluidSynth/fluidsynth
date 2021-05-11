ad3f796aaa84adf92982dab98c6bfbd271b89d3b
derselbst <tom.mbrt@googlemail.com>
2021-05-11 22:34:24 +0200

    add more test cases for UTF8 file names

--- test/test_utf8_open.c
+++ test/test_utf8_open.c
@@ -11,11 +11,20 @@ int main(void)
     int id;
     fluid_settings_t *settings;
     fluid_synth_t *synth;
+    fluid_player_t *player;
 
-	FILE *sfont_file;
-    sfont_file = FLUID_FOPEN(TEST_SOUNDFONT_UTF8, "rb");
-    TEST_ASSERT(sfont_file != NULL);
-    TEST_ASSERT(FLUID_FCLOSE(sfont_file) == 0);
+    FILE *file;
+    file = FLUID_FOPEN(TEST_SOUNDFONT_UTF8_1, "rb");
+    TEST_ASSERT(file != NULL);
+    TEST_ASSERT(FLUID_FCLOSE(file) == 0);
+
+    file = FLUID_FOPEN(TEST_SOUNDFONT_UTF8_2, "rb");
+    TEST_ASSERT(file != NULL);
+    TEST_ASSERT(FLUID_FCLOSE(file) == 0);
+
+    file = FLUID_FOPEN(TEST_MIDI_UTF8, "rb");
+    TEST_ASSERT(file != NULL);
+    TEST_ASSERT(FLUID_FCLOSE(file) == 0);
 
     settings = new_fluid_settings();
     synth = new_fluid_synth(settings);
@@ -26,11 +35,19 @@ int main(void)
     // no sfont loaded
     TEST_ASSERT(fluid_synth_sfcount(synth) == 0);
 
-    TEST_ASSERT(fluid_is_soundfont(TEST_SOUNDFONT_UTF8) == TRUE);
+    TEST_ASSERT(fluid_is_soundfont(TEST_SOUNDFONT_UTF8_1) == TRUE);
+    TEST_SUCCESS(id = fluid_synth_sfload(synth, TEST_SOUNDFONT_UTF8_1, 1));
 
-    // load a sfont to synth
-    TEST_SUCCESS(id = fluid_synth_sfload(synth, TEST_SOUNDFONT_UTF8, 1));
+    TEST_ASSERT(fluid_is_soundfont(TEST_SOUNDFONT_UTF8_2) == TRUE);
+    TEST_SUCCESS(id = fluid_synth_sfload(synth, TEST_SOUNDFONT_UTF8_2, 1));
 
+    player = new_fluid_player(synth);
+    TEST_ASSERT(player != NULL);
+
+    TEST_ASSERT(fluid_is_midifile(TEST_MIDI_UTF8) == TRUE);
+    TEST_SUCCESS(fluid_player_add(player, TEST_MIDI_UTF8));
+
+    delete_fluid_player(player);
     delete_fluid_synth(synth);
     delete_fluid_settings(settings);
 
