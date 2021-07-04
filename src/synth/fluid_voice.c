e096919477234baafffca4464a4e28924f47fb74
Tamás Korodi <tamaskorodi1988@gmail.com>
2021-07-05 00:47:47 +0200

    Select soundfont samples by frequency instead of midi note numbers (#932)
    For detuned channels it might be better to use another key for Soundfont sample selection
    giving better approximations for the pitch than the original key.
    Example: play key 60 on 6370 Hz => use tuned key 64 for sample selection
    
    This feature is only enabled for melodic channels.
    For drum channels we always select Soundfont samples by key numbers.

--- src/synth/fluid_voice.c
+++ src/synth/fluid_voice.c
@@ -670,6 +670,7 @@ calculate_hold_decay_buffers(fluid_voice_t *voice, int gen_base,
      * GEN_KEYTOVOLENVDECAY, GEN_KEYTOMODENVHOLD, GEN_KEYTOMODENVDECAY
      */
 
+    fluid_real_t keysteps;
     fluid_real_t timecents;
     fluid_real_t seconds;
     int buffers;
@@ -681,7 +682,9 @@ calculate_hold_decay_buffers(fluid_voice_t *voice, int gen_base,
      * will cause (60-72)*100=-1200 timecents of time variation.
      * The time is cut in half.
      */
-    timecents = (fluid_voice_gen_value(voice, gen_base) + fluid_voice_gen_value(voice, gen_key2base) * (fluid_real_t)(60 - fluid_voice_get_actual_key(voice)));
+
+    keysteps = 60.0f - fluid_channel_get_key_pitch(voice->channel, fluid_voice_get_actual_key(voice)) / 100.0f;
+    timecents = fluid_voice_gen_value(voice, gen_base) + fluid_voice_gen_value(voice, gen_key2base) * keysteps;
 
     /* Range checking */
     if(is_decay)
