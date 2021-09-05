5a71336bd991ebf3106359a22b01ffc715fab872
Pedro López-Cabanillas <plcl@users.sourceforge.net>
2021-09-05 11:30:25 +0200

    replaced global include_directories and link_directories
    * by target_include_directories
    * by target_link_libraries, using imported targets when possible

--- cmake_admin/FluidUnitTest.cmake
+++ cmake_admin/FluidUnitTest.cmake
@@ -16,7 +16,7 @@ macro ( ADD_FLUID_TEST _test )
     $<BUILD_INTERFACE:${CMAKE_BINARY_DIR}/include> # include auto generated headers
     $<BUILD_INTERFACE:${CMAKE_SOURCE_DIR}/include> # include "normal" public (sub-)headers
     $<BUILD_INTERFACE:${CMAKE_SOURCE_DIR}/src> # include private headers
-    $<TARGET_PROPERTY:libfluidsynth,INCLUDE_DIRECTORIES> # include all other header search paths needed by libfluidsynth (esp. glib)
+    $<TARGET_PROPERTY:libfluidsynth-OBJ,INCLUDE_DIRECTORIES> # include all other header search paths needed by libfluidsynth (esp. glib)
     )
 
     # add the test to ctest
@@ -48,7 +48,7 @@ macro ( ADD_FLUID_TEST_UTIL _util )
     $<BUILD_INTERFACE:${CMAKE_BINARY_DIR}/include> # include auto generated headers
     $<BUILD_INTERFACE:${CMAKE_SOURCE_DIR}/include> # include "normal" public (sub-)headers
     $<BUILD_INTERFACE:${CMAKE_SOURCE_DIR}/src> # include private headers
-    $<TARGET_PROPERTY:libfluidsynth,INCLUDE_DIRECTORIES> # include all other header search paths needed by libfluidsynth (esp. glib)
+    $<TARGET_PROPERTY:libfluidsynth-OBJ,INCLUDE_DIRECTORIES> # include all other header search paths needed by libfluidsynth (esp. glib)
     )
 
     # append the current unit test to check-target as dependency
