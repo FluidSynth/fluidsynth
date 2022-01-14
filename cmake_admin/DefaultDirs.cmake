8a96ec9c3a8de78a5fcf638e9a1e45f1a9969d26
Pedro López-Cabanillas <plcl@users.sf.net>
2022-01-14 19:56:50 +0100

    cmake macos frameworks update
    The framework target needs to know about the headers, so cmake tries to copy the framework files into the bundle.
    The macro set_source_files_properties() is a modern (cmake >= 3.0) replacement of the general set_property().

--- cmake_admin/DefaultDirs.cmake
+++ cmake_admin/DefaultDirs.cmake
@@ -12,12 +12,12 @@ endif ( WIN32 )
 mark_as_advanced (DEFAULT_SOUNDFONT)
 
 # BUNDLE_INSTALL_DIR - Mac only: the directory for application bundles 
-set (BUNDLE_INSTALL_DIR "/Applications" CACHE STRING 
+set (BUNDLE_INSTALL_DIR "Applications" CACHE STRING
      "The install dir for application bundles")
 mark_as_advanced (BUNDLE_INSTALL_DIR)
      
 # FRAMEWORK_INSTALL_DIR - Mac only: the directory for framework bundles
-set (FRAMEWORK_INSTALL_DIR "/Library/Frameworks" CACHE STRING 
+set (FRAMEWORK_INSTALL_DIR "Library/Frameworks" CACHE STRING
      "The install dir for framework bundles")
 mark_as_advanced (FRAMEWORK_INSTALL_DIR) 
 
