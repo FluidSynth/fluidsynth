9bbe7d54c03b7362d90fd21b9405c46c557cc820
derselbst <tom.mbrt@googlemail.com>
2024-10-31 13:57:45 +0100

    Revert synth.gain to its old default value
    Fixes #1405

--- src/synth/fluid_synth.c
+++ src/synth/fluid_synth.c
@@ -224,7 +224,7 @@ void fluid_synth_settings(fluid_settings_t *settings)
 
     fluid_settings_register_int(settings, "synth.polyphony", 256, 1, 65535, 0);
     fluid_settings_register_int(settings, "synth.midi-channels", 16, 16, 256, 0);
-    fluid_settings_register_num(settings, "synth.gain", 0.6, 0.0, 10.0, 0);
+    fluid_settings_register_num(settings, "synth.gain", 0.2, 0.0, 10.0, 0);
     fluid_settings_register_int(settings, "synth.audio-channels", 1, 1, 128, 0);
     fluid_settings_register_int(settings, "synth.audio-groups", 1, 1, 128, 0);
     fluid_settings_register_int(settings, "synth.effects-channels", 2, 2, 2, 0);
