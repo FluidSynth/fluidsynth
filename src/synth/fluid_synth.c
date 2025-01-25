ab6e0dd8cffa3573192ebae8a93b9e0d554889ff
Tom M. <tom.mbrt@googlemail.com>
2025-01-25 11:12:43 +0100

    Make MSGS-style note-cut configurable (#1471)

--- src/synth/fluid_synth.c
+++ src/synth/fluid_synth.c
@@ -256,6 +256,7 @@ void fluid_synth_settings(fluid_settings_t *settings)
     fluid_settings_add_option(settings, "synth.midi-bank-select", "mma");
 
     fluid_settings_register_int(settings, "synth.dynamic-sample-loading", 0, 0, 1, FLUID_HINT_TOGGLED);
+    fluid_settings_register_int(settings, "synth.note-cut", 0, 0, 2, 0);
 }
 
 /**
@@ -687,6 +688,9 @@ new_fluid_synth(fluid_settings_t *settings)
     fluid_settings_getnum_float(settings, "synth.overflow.volume", &synth->overflow.volume);
     fluid_settings_getnum_float(settings, "synth.overflow.age", &synth->overflow.age);
     fluid_settings_getnum_float(settings, "synth.overflow.important", &synth->overflow.important);
+    
+    fluid_settings_getint(settings, "synth.note-cut", &i);
+    synth->msgs_note_cut_mode = i;
 
     /* register the callbacks */
     fluid_settings_callback_num(settings, "synth.gain",
@@ -6878,6 +6882,7 @@ fluid_synth_release_voice_on_same_note_LOCAL(fluid_synth_t *synth, int chan,
                 && (fluid_voice_get_id(voice) != synth->noteid))
         {
             enum fluid_midi_channel_type type = synth->channel[chan]->channel_type;
+            enum fluid_msgs_note_cut note_cut_mode = synth->msgs_note_cut_mode;
 
             /* Id of voices that was sustained by sostenuto */
             if(fluid_voice_is_sostenuto(voice))
@@ -6885,21 +6890,17 @@ fluid_synth_release_voice_on_same_note_LOCAL(fluid_synth_t *synth, int chan,
                 synth->storeid = fluid_voice_get_id(voice);
             }
 
-            switch(type)
+            if(note_cut_mode == FLUID_MSGS_DISABLED || (note_cut_mode == FLUID_MSGS_DRUM_CUT && type == CHANNEL_TYPE_MELODIC))
             {
-                case CHANNEL_TYPE_DRUM:
-                    /* release the voice, this should make riding hi-hats or snares sound more
-                     * realistic (Discussion #1196) */
-                    fluid_voice_off(voice);
-                    break;
-                case CHANNEL_TYPE_MELODIC:
-                    /* Force the voice into release stage except if pedaling (sostenuto or sustain) is active.
-                     * This gives a more realistic sound to pianos and possibly other instruments (see PR #905). */
-                    fluid_voice_noteoff(voice);
-                    break;
-                default:
-                    FLUID_LOG(FLUID_ERR, "This should never happen: unknown channel type %d", (int)type);
-                    break;
+                /* Force the voice into release stage except if pedaling (sostenuto or sustain) is active.
+                 * This gives a more realistic sound to pianos and possibly other instruments (see PR #905). */
+                fluid_voice_noteoff(voice);
+            }
+            else // i.e. if((note_cut_mode == FLUID_MSGS_ALL_CUT) || (note_cut_mode == FLUID_MSGS_DRUM_CUT && type == CHANNEL_TYPE_DRUM))
+            {
+                /* release the voice, this should make riding hi-hats or snares sound more realistic (Discussion #1196)
+                 * Note however, that this is not a SF2 compliant behavior, see #1466 */
+                fluid_voice_off(voice);
             }
         }
     }
