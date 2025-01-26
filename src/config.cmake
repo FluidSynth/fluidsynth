63f66ecf8edbeb9e68d6cedfac319b28ee730dd4
Andy Vandijck <andyvand@gmail.com>
2025-01-26 15:39:38 +0100

    SDL3 Support
    Add SDL3 support

--- src/config.cmake
+++ src/config.cmake
@@ -208,6 +208,9 @@
 /* Define to enable SDL2 audio driver */
 #cmakedefine SDL2_SUPPORT @SDL2_SUPPORT@
 
+/* Define to enable SDL3 audio driver */
+#cmakedefine SDL3_SUPPORT @SDL3_SUPPORT@
+
 /* Define to 1 if you have the ANSI C header files. */
 #cmakedefine STDC_HEADERS @STDC_HEADERS@
 
