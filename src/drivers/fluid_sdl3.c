bc33fa435be66f272495c2d9c50f98d587a733e9
Andy Vandijck <andyvand@gmail.com>
2025-02-08 08:51:31 +0100

    Fix SDL buffer length
    Fix SDL buffer length

--- src/drivers/fluid_sdl3.c
+++ src/drivers/fluid_sdl3.c
@@ -161,7 +161,7 @@ SDLAudioCallback(void *data, SDL_AudioStream *stream, int add_len, int len)
 
     while (add_len > 0)
     {
-        buf_len = SDL_min(add_len, (int)SDL_arraysize(buffer));
+        buf_len = SDL_min(add_len, dev->period_size * dev->frame_size);
         dev->write_ptr(dev->synth, buf_len / dev->frame_size, buffer, 0, 2, buffer, 1, 2);
         SDL_PutAudioStreamData(stream, buffer, buf_len);
         add_len -= buf_len;  /* subtract what we've just fed the stream. */
