cb2ecb9b087d6093f8f94571f44044f75843de5f
Michael Lotz <mmlr@mlotz.ch>
2025-05-02 11:18:15 +0200

    Use abstracted fluid_file_test() and constant in one more place.
    The code right below already used it, but this one appears to have been
    missed.

--- src/fluidsynth.c
+++ src/fluidsynth.c
@@ -892,7 +892,7 @@ int main(int argc, char **argv)
     if(config_file == NULL)
     {
         config_file = fluid_get_userconf(buf, sizeof(buf));
-        if(config_file == NULL || !g_file_test(config_file, G_FILE_TEST_EXISTS))
+        if(config_file == NULL || !fluid_file_test(config_file, FLUID_FILE_TEST_EXISTS))
         {
             config_file = fluid_get_sysconf(buf, sizeof(buf));
         }
