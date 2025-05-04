fc64319a8c20c3921e5bf2e7f8b6149ef68466ca
derselbst <tom.mbrt@googlemail.com>
2025-05-04 16:18:40 +0200

    undo some changes

--- src/rvoice/fluid_iir_filter_impl.cpp
+++ src/rvoice/fluid_iir_filter_impl.cpp
@@ -304,7 +304,7 @@ void fluid_iir_filter_calc(fluid_iir_filter_t *iir_filter,
     }
 
     /* calculate the frequency of the resonant filter in Hz */
-    fres = fluid_ct2hz(iir_filter->fres + fres_mod) + 10;
+    fres = fluid_ct2hz(iir_filter->fres + fres_mod);
 
     /* I removed the optimization of turning the filter off when the
      * resonance frequency is above the maximum frequency. Instead, the
