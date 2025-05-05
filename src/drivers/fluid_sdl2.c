65de764247f8e110b719ea6518e96637577ac38d
Benjamin A. Beasley <code@musicinmybrain.net>
2025-05-04 22:38:42 -0400

    Update LGPL-2.1-or-later license notices
    The FSF is now remote-only and no longer has a physical address; see
    https://www.gnu.org/licenses/old-licenses/lgpl-2.1.en.html#SEC4.

--- src/drivers/fluid_sdl2.c
+++ src/drivers/fluid_sdl2.c
@@ -14,9 +14,8 @@
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free
- * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
- * 02110-1301, USA
+ * License along with this library; if not, see
+ * <https://www.gnu.org/licenses/>.
  */
 
 #include "fluid_synth.h"
