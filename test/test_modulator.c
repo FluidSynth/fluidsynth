f050039373803f10a38cb864168c28abb225d555
derselbst <tom.mbrt@googlemail.com>
2025-09-28 12:09:38 +0200

    Refactor modulator unit test, also test no sources

--- test/test_modulator.c
+++ test/test_modulator.c
@@ -10,41 +10,30 @@
 // Range shall be set to 7bit resolution, i.e. 128
 static const fluid_real_t Range = 128.0;
 
-fluid_real_t get_mod_max(const fluid_mod_t *mod)
+static const int Mapping[] = {FLUID_MOD_SWITCH, FLUID_MOD_LINEAR, FLUID_MOD_CONCAVE, FLUID_MOD_CONVEX};
+static const int Polar[] = {FLUID_MOD_UNIPOLAR, FLUID_MOD_BIPOLAR};
+static const int Direction[] = {FLUID_MOD_POSITIVE, FLUID_MOD_NEGATIVE};
+
+static fluid_real_t get_mod_max(const fluid_mod_t *mod)
 {
     // The maximum mapped position is always 127/128, see section 9.5.3 of SF2.4
     // For switches however, we keep sticking to fluidsynth historical limits of +-1.0
     return ((mod->flags1 & FLUID_MOD_MAP_MASK) == FLUID_MOD_SWITCH) ? 1.0f : (Range-1)/Range;
 }
 
-// this tests ensures that samples with invalid SfSampleType flag combinations are rejected
-int main(void)
+static void test_mod_source_mapping(fluid_mod_t *mod)
 {
-    static const int mapping[] = {FLUID_MOD_SWITCH, FLUID_MOD_LINEAR, FLUID_MOD_CONCAVE, FLUID_MOD_CONVEX};
     unsigned int i;
     fluid_real_t v1, tmp;
 
-    fluid_mod_t* mod = new_fluid_mod();
-    fluid_mod_set_source2(mod, 0, 0);
-    fluid_mod_set_dest(mod, GEN_ATTENUATION);
-    fluid_mod_set_amount(mod, 1);
-
-    // No secondary source given, result must be one
-    tmp = Range;
-    v1 = fluid_mod_get_source_value(mod->src2, mod->flags2, &tmp, NULL);
-    TEST_ASSERT(tmp == Range);
-    TEST_ASSERT(v1 == Range);
-    v1 = fluid_mod_transform_source_value(mod, v1, mod->flags2, Range, TRUE);
-    TEST_ASSERT(v1 == 1.0f);
-
-    for(i = 0; i < FLUID_N_ELEMENTS(mapping); i++)
+    for(i = 0; i < FLUID_N_ELEMENTS(Mapping); i++)
     {
         {
             static const fluid_real_t mid = 64.0/128.0;
             fluid_mod_set_source1(mod,
                             FLUID_MOD_VELOCITY,
                             FLUID_MOD_GC
-                            | mapping[i]
+                            | Mapping[i]
                             | FLUID_MOD_UNIPOLAR
                             | FLUID_MOD_POSITIVE
                             );
@@ -53,7 +42,7 @@ int main(void)
             TEST_ASSERT(v1 == 0.0f);
 
             // skip midpoint validation for concave and convex since we're not checking correctness of concave and convex implementations here
-            if(mapping[i] != FLUID_MOD_CONCAVE && mapping[i] != FLUID_MOD_CONVEX)
+            if(Mapping[i] != FLUID_MOD_CONCAVE && Mapping[i] != FLUID_MOD_CONVEX)
             {
                 v1 = fluid_mod_transform_source_value(mod, 64, mod->flags1, Range, TRUE);
                 tmp = ((mod->flags1 & FLUID_MOD_MAP_MASK) == FLUID_MOD_SWITCH) ? 1.0f : mid;
@@ -70,7 +59,7 @@ int main(void)
             fluid_mod_set_source1(mod,
                             FLUID_MOD_VELOCITY,
                             FLUID_MOD_GC
-                            | mapping[i]
+                            | Mapping[i]
                             | FLUID_MOD_UNIPOLAR
                             | FLUID_MOD_NEGATIVE
                             );
@@ -78,7 +67,7 @@ int main(void)
             v1 = fluid_mod_transform_source_value(mod, 127, mod->flags1, Range, TRUE);
             TEST_ASSERT(v1 == 0.0f);
 
-            if(mapping[i] != FLUID_MOD_CONCAVE && mapping[i] != FLUID_MOD_CONVEX)
+            if(Mapping[i] != FLUID_MOD_CONCAVE && Mapping[i] != FLUID_MOD_CONVEX)
             {
                 v1 = fluid_mod_transform_source_value(mod, 64, mod->flags1, Range, TRUE);
                 tmp = ((mod->flags1 & FLUID_MOD_MAP_MASK) == FLUID_MOD_SWITCH) ? 0.0f : mid;
@@ -95,7 +84,7 @@ int main(void)
             fluid_mod_set_source1(mod,
                             FLUID_MOD_VELOCITY,
                             FLUID_MOD_GC
-                            | mapping[i]
+                            | Mapping[i]
                             | FLUID_MOD_BIPOLAR
                             | FLUID_MOD_POSITIVE
                             );
@@ -103,7 +92,7 @@ int main(void)
             v1 = fluid_mod_transform_source_value(mod, 0, mod->flags1, Range, TRUE);
             TEST_ASSERT(v1 == -1.0f);
 
-            if(mapping[i] != FLUID_MOD_CONCAVE && mapping[i] != FLUID_MOD_CONVEX)
+            if(Mapping[i] != FLUID_MOD_CONCAVE && Mapping[i] != FLUID_MOD_CONVEX)
             {
                 v1 = fluid_mod_transform_source_value(mod, 64, mod->flags1, Range, TRUE);
                 tmp = ((mod->flags1 & FLUID_MOD_MAP_MASK) == FLUID_MOD_SWITCH) ? 1.0f : mid;
@@ -120,7 +109,7 @@ int main(void)
             fluid_mod_set_source1(mod,
                             FLUID_MOD_VELOCITY,
                             FLUID_MOD_GC
-                            | mapping[i]
+                            | Mapping[i]
                             | FLUID_MOD_BIPOLAR
                             | FLUID_MOD_NEGATIVE
                             );
@@ -128,7 +117,7 @@ int main(void)
             v1 = fluid_mod_transform_source_value(mod, 127, mod->flags1, Range, TRUE);
             TEST_ASSERT(v1 == -1.0f);
 
-            if(mapping[i] != FLUID_MOD_CONCAVE && mapping[i] != FLUID_MOD_CONVEX)
+            if(Mapping[i] != FLUID_MOD_CONCAVE && Mapping[i] != FLUID_MOD_CONVEX)
             {
                 v1 = fluid_mod_transform_source_value(mod, 64, mod->flags1, Range, TRUE);
                 tmp = ((mod->flags1 & FLUID_MOD_MAP_MASK) == FLUID_MOD_SWITCH) ? -1.0f : mid;
@@ -140,6 +129,38 @@ int main(void)
             TEST_ASSERT(v1 == tmp);
         }
     }
+}
+
+static void test_mod_no_source(fluid_mod_t *mod)
+{
+    unsigned int i,j,k;
+    fluid_real_t v1, tmp;
+    fluid_mod_set_dest(mod, GEN_ATTENUATION);
+    fluid_mod_set_amount(mod, 1);
+
+    for(i = 0; i < FLUID_N_ELEMENTS(Mapping); i++)
+    for(j = 0; j < FLUID_N_ELEMENTS(Polar); j++)
+    for(k = 0; k < FLUID_N_ELEMENTS(Direction); k++)
+    {
+        fluid_mod_set_source2(mod, FLUID_MOD_NONE, Mapping[i] | Polar[j] | Direction[k]);
+        // No secondary source given, result must be one
+        tmp = Range;
+        v1 = fluid_mod_get_source_value(mod->src2, mod->flags2, &tmp, NULL);
+        TEST_ASSERT(tmp == Range);
+        TEST_ASSERT(v1 == Range);
+        v1 = fluid_mod_transform_source_value(mod, v1, mod->flags2, Range, TRUE);
+        TEST_ASSERT(v1 == 1.0f);
+    }
+}
+
+// this tests ensures that samples with invalid SfSampleType flag combinations are rejected
+int main(void)
+{
+    fluid_mod_t* mod = new_fluid_mod();
+
+    test_mod_no_source(mod);
+
+    test_mod_source_mapping(mod);
 
     delete_fluid_mod(mod);
 
