3bf3d7a7c0d7f9d997c1ea5a5ae4ab14f3726f1f
Anupam Basak <anupam.basak27@gmail.com>
2026-09-03 23:57:32 +0530

    Allow setting pipewire node name (#1842)
    Like the jack driver, pipewire supports setting a device name. Introduce a new settings entry named audio.pipewire.id, which sets the pipewire node name property.
    
    Co-authored-by: Tom M. <tom.mbrt@googlemail.com>

--- src/drivers/fluid_pipewire.c
+++ src/drivers/fluid_pipewire.c
@@ -202,6 +202,7 @@ new_fluid_pipewire_audio_driver2(fluid_settings_t *settings, fluid_audio_func_t 
     char *media_role = NULL;
     char *media_type = NULL;
     char *media_category = NULL;
+    char *client_name = NULL;
     float *buffer = NULL;
     const struct spa_pod *params[1];
     struct pw_properties *props;
@@ -224,6 +225,7 @@ new_fluid_pipewire_audio_driver2(fluid_settings_t *settings, fluid_audio_func_t 
     fluid_settings_dupstr(settings, "audio.pipewire.media-role", &media_role);
     fluid_settings_dupstr(settings, "audio.pipewire.media-type", &media_type);
     fluid_settings_dupstr(settings, "audio.pipewire.media-category", &media_category);
+    fluid_settings_dupstr(settings, "audio.pipewire.id", &client_name);
 
     max_latency_frames = period_size * periods;
 
@@ -273,7 +275,7 @@ new_fluid_pipewire_audio_driver2(fluid_settings_t *settings, fluid_audio_func_t 
     pw_core_add_listener(drv->pw_core, &drv->core_listener,
                             &core_events, NULL);
 
-    props = pw_properties_new(PW_KEY_MEDIA_TYPE, media_type, PW_KEY_MEDIA_CATEGORY, media_category, PW_KEY_MEDIA_ROLE, media_role, NULL);
+    props = pw_properties_new(PW_KEY_MEDIA_TYPE, media_type, PW_KEY_MEDIA_CATEGORY, media_category, PW_KEY_MEDIA_ROLE, media_role, PW_KEY_NODE_NAME, client_name, NULL);
     pw_properties_setf(props, PW_KEY_NODE_LATENCY, "%d/%d", max_latency_frames, (int) sample_rate);
     pw_properties_setf(props, PW_KEY_NODE_RATE, "1/%d", (int) sample_rate);
     if(async)
@@ -361,6 +363,7 @@ new_fluid_pipewire_audio_driver2(fluid_settings_t *settings, fluid_audio_func_t 
     FLUID_FREE(media_role);
     FLUID_FREE(media_type);
     FLUID_FREE(media_category);
+    FLUID_FREE(client_name);
 
     return (fluid_audio_driver_t *)drv;
 
@@ -368,6 +371,7 @@ driver_cleanup:
     FLUID_FREE(media_role);
     FLUID_FREE(media_type);
     FLUID_FREE(media_category);
+    FLUID_FREE(client_name);
 
     delete_fluid_pipewire_audio_driver((fluid_audio_driver_t *)drv);
     return NULL;
@@ -424,6 +428,7 @@ void fluid_pipewire_audio_driver_settings(fluid_settings_t *settings)
     fluid_settings_register_str(settings, "audio.pipewire.media-role", "Music", 0);
     fluid_settings_register_str(settings, "audio.pipewire.media-type", "Audio", 0);
     fluid_settings_register_str(settings, "audio.pipewire.media-category", "Playback", 0);
+    fluid_settings_register_str(settings, "audio.pipewire.id", "fluidsynth", 0);
     fluid_settings_register_int(settings, "audio.pipewire.async", 1, 0, 1, 0);
 }
 
